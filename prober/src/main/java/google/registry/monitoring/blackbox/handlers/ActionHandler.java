2243256ab694d85f02b6b5cabfa1787e553f945b
Aman Sanger <sangera@google.com>
2019-06-24 12:18:55 -0400

    Added Protocol Class and its Basic Unit Tests

--- prober/src/main/java/google/registry/monitoring/blackbox/handlers/ActionHandler.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/handlers/ActionHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2019 The Nomulus Authors. All Rights Reserved.
+// Copyright 2017 The Nomulus Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,68 +14,56 @@
 
 package google.registry.monitoring.blackbox.handlers;
 
-import com.google.common.flogger.FluentLogger;
-import google.registry.monitoring.blackbox.messages.InboundMessageType;
-import google.registry.monitoring.blackbox.messages.OutboundMessageType;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import io.netty.channel.SimpleChannelInboundHandler;
+import java.util.function.Function;
+import javax.inject.Inject;
 
-/**
- * Superclass of all {@link ChannelHandler}s placed at end of channel pipeline
- *
- * <p>{@code ActionHandler} inherits from {@link SimpleChannelInboundHandler< InboundMessageType >},
- * as it should only be passed in messages that implement the {@link InboundMessageType} interface.
- *
- * <p>The {@code ActionHandler} skeleton exists for a few main purposes. First, it returns a {@link
- * ChannelPromise}, which informs the {@link ProbingAction} in charge that a response has been read.
- * Second, it stores the {@link OutboundMessageType} passed down the pipeline, so that subclasses
- * can use that information for their own processes. Lastly, with any exception thrown, the
- * connection is closed, and the ProbingAction governing this channel is informed of the error.
- * Subclasses specify further work to be done for specific kinds of channel pipelines.
- */
-public abstract class ActionHandler extends SimpleChannelInboundHandler<InboundMessageType> {
+public class ActionHandler extends SimpleChannelInboundHandler<Object>
+    implements Function<Object, ChannelFuture> {
 
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private ChannelPromise finished;
+  private Channel channel;
 
-  protected ChannelPromise finished;
+  @Inject
+  public ActionHandler() {}
 
-  /**
-   * Takes in {@link OutboundMessageType} type and saves for subclasses. Then returns initialized
-   * {@link ChannelPromise}
-   */
-  public ChannelFuture getFuture() {
+  @Override
+  public ChannelFuture apply(Object outboundMessage) {
+    // Sends request along Outbound Handlers on the Pipeline
+
+    channel.writeAndFlush(outboundMessage);
     return finished;
   }
 
-  /** Initializes new {@link ChannelPromise} */
   @Override
   public void handlerAdded(ChannelHandlerContext ctx) {
-    // Once handler is added to channel pipeline, initialize channel and future for this handler
+    //Once handler is added to channel pipeline, initialize channel and future for this handler
+    channel = ctx.channel();
     finished = ctx.newPromise();
   }
 
   @Override
-  public void channelRead0(ChannelHandlerContext ctx, InboundMessageType inboundMessage)
-      throws Exception {
-    // simply marks finished as success
-    finished = finished.setSuccess();
+  protected void channelRead0(ChannelHandlerContext ctx, Object InboundMessage) {
+    //Only purpose of Handler is to mark future as a success
+
+    finished.setSuccess();
   }
 
   /**
-   * Logs the channel and pipeline that caused error, closes channel, then informs {@link
-   * ProbingAction} listeners of error
+   *Both methods are only used for testing
    */
-  @Override
-  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-    logger.atSevere().withCause(cause).log(
-        String.format(
-            "Attempted Action was unsuccessful with channel: %s, having pipeline: %s",
-            ctx.channel().toString(), ctx.channel().pipeline().toString()));
 
-    finished = finished.setFailure(cause);
-    ChannelFuture closedFuture = ctx.channel().close();
-    closedFuture.addListener(f -> logger.atInfo().log("Unsuccessful channel connection closed"));
+  public Channel getChannel() {
+    return channel;
   }
+
+  public ChannelPromise getFinished() {
+    return finished;
+  }
+
 }
+
