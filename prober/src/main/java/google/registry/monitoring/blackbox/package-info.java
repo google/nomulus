26d8ebec73fd818f2c8127b96350e9b703256975
Aman Sanger <sangera@google.com>
2019-06-28 17:10:59 -0400

    Added Changes Suggested by jianglai

--- prober/src/main/java/google/registry/monitoring/blackbox/package-info.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/package-info.java
@@ -12,5 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@javax.annotation.ParametersAreNonnullByDefault
 package google.registry.monitoring.blackbox;
