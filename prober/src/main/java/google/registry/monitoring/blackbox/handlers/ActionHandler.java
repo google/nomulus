e241adb8fe3ec9d8bd39ddb6aa4e07f1c8d194a6
Aman Sanger <sangera@google.com>
2019-06-28 17:10:59 -0400

    Added Changes Suggested by jianglai

--- prober/src/main/java/google/registry/monitoring/blackbox/handlers/ActionHandler.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/handlers/ActionHandler.java
@@ -14,56 +14,73 @@
 
 package google.registry.monitoring.blackbox.handlers;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.common.flogger.StackSize;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.http.HttpResponse;
 import java.util.function.Function;
 import javax.inject.Inject;
 
-public class ActionHandler extends SimpleChannelInboundHandler<Object>
-    implements Function<Object, ChannelFuture> {
+/**
+ *
+ * @param <I> Generic Type of Inbound Message
+ * @param <O> Generic Type of Outbound Message
+ * Abstract class that tells sends message down pipeline and
+ * and tells listeners to move on when the message is received.
+ */
+public abstract class ActionHandler<I, O> extends SimpleChannelInboundHandler<I>
+    implements Function<O, ChannelFuture> {
 
-  private ChannelPromise finished;
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  protected ChannelPromise finished;
   private Channel channel;
 
-  @Inject
-  public ActionHandler() {}
 
+  /** returns ChannelPromise for when inbound message is recieved
+   * a
+   * @param outboundMessage
+   * @return
+   */
   @Override
-  public ChannelFuture apply(Object outboundMessage) {
+  public ChannelFuture apply(O outboundMessage) {
     // Sends request along Outbound Handlers on the Pipeline
 
     channel.writeAndFlush(outboundMessage);
     return finished;
+
+  }
+
+  public void resetFuture() {
+    finished = channel.newPromise();
   }
 
   @Override
   public void handlerAdded(ChannelHandlerContext ctx) {
     //Once handler is added to channel pipeline, initialize channel and future for this handler
+
     channel = ctx.channel();
     finished = ctx.newPromise();
   }
 
   @Override
-  protected void channelRead0(ChannelHandlerContext ctx, Object InboundMessage) {
+  public void channelRead0(ChannelHandlerContext ctx, I inboundMessage) throws Exception{
     //Only purpose of Handler is to mark future as a success
 
     finished.setSuccess();
   }
 
-  /**
-   *Both methods are only used for testing
-   */
-
-  public Channel getChannel() {
-    return channel;
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    logger.atSevere().withCause(cause).withStackTrace(StackSize.FULL).log("Exception Caught");
   }
 
-  public ChannelPromise getFinished() {
-    return finished;
-  }
+
 
 }
 
