f1be6a0b7ef99bec093a462410dd0a3f706efc05
Aman Sanger <sangera@google.com>
2019-06-28 17:10:59 -0400

    Added Changes Suggested by jianglai

--- prober/src/test/java/google/registry/monitoring/blackbox/Handlers/ActionHandlerTest.java
+++ prober/src/test/java/google/registry/monitoring/blackbox/Handlers/ActionHandlerTest.java
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package google.registry.monitoring.blackbox;
+package google.registry.monitoring.blackbox.Handlers;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import google.registry.monitoring.blackbox.handlers.ActionHandler;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelPromise;
