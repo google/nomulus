7836c3f704486ba5efec1d416569260895da6442
Aman Sanger <sangera@google.com>
2019-07-01 16:56:54 -0400

    Added Ssl and WebWhois Action Handlers and their unit tests in addition to the ProbingAction class

--- prober/src/main/java/google/registry/monitoring/blackbox/handlers/SslClientInitializer.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/handlers/SslClientInitializer.java
@@ -1,4 +1,4 @@
-// Copyright 2019 The Nomulus Authors. All Rights Reserved.
+// Copyright 2017 The Nomulus Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,11 +15,12 @@
 package google.registry.monitoring.blackbox.handlers;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static google.registry.monitoring.blackbox.ProbingAction.REMOTE_ADDRESS_KEY;
 import static google.registry.monitoring.blackbox.Protocol.PROTOCOL_KEY;
+
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.flogger.FluentLogger;
+
 import google.registry.monitoring.blackbox.Protocol;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler.Sharable;
@@ -28,18 +29,14 @@ import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.ssl.SslProvider;
-import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
-import java.util.function.Supplier;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLParameters;
 
 /**
  * Adds a client side SSL handler to the channel pipeline.
- *
- * <p> Code is close to unchanged from {@link SslClientInitializer}</p> in proxy, but is modified
- * for revised overall structure of connections, and to accomdate EPP connections </p>
  *
  * <p>This <b>must</b> be the first handler provided for any handler provider list, if it is
  * provided. The type parameter {@code C} is needed so that unit tests can construct this handler
@@ -53,59 +50,30 @@ public class SslClientInitializer<C extends Channel> extends ChannelInitializer<
 
   private final SslProvider sslProvider;
   private final X509Certificate[] trustedCertificates;
-  private final Supplier<PrivateKey> privateKeySupplier;
-  private final Supplier<X509Certificate[]> certificateSupplier;
 
-
+  @Inject
   public SslClientInitializer(SslProvider sslProvider) {
     // null uses the system default trust store.
-    //Used for WebWhois, so we don't care about privateKey and certificates, setting them to null
-    this(sslProvider, null, null, null);
-  }
-
-  public SslClientInitializer(SslProvider sslProvider, Supplier<PrivateKey> privateKeySupplier,
-      Supplier<X509Certificate[]> certificateSupplier) {
-    //We use the default trust store here as well, setting trustCertificates to null
-    this(sslProvider, null, privateKeySupplier, certificateSupplier);
+    this(sslProvider, null);
   }
 
   @VisibleForTesting
   SslClientInitializer(SslProvider sslProvider, X509Certificate[] trustCertificates) {
-    this(sslProvider, trustCertificates, null, null);
-  }
-
-  private SslClientInitializer(
-      SslProvider sslProvider,
-      X509Certificate[] trustCertificates,
-      Supplier<PrivateKey> privateKeySupplier,
-      Supplier<X509Certificate[]> certificateSupplier) {
     logger.atInfo().log("Client SSL Provider: %s", sslProvider);
-
     this.sslProvider = sslProvider;
     this.trustedCertificates = trustCertificates;
-    this.privateKeySupplier = privateKeySupplier;
-    this.certificateSupplier = certificateSupplier;
   }
 
   @Override
   protected void initChannel(C channel) throws Exception {
     Protocol protocol = channel.attr(PROTOCOL_KEY).get();
-    String host = channel.attr(REMOTE_ADDRESS_KEY).get();
-
-    //Builds SslHandler from Protocol, and based on if we require a privateKey and certificate
     checkNotNull(protocol, "Protocol is not set for channel: %s", channel);
-    SslContextBuilder sslContextBuilder =
+    SslHandler sslHandler =
         SslContextBuilder.forClient()
             .sslProvider(sslProvider)
-            .trustManager(trustedCertificates);
-    if (privateKeySupplier != null && certificateSupplier != null) {
-      sslContextBuilder = sslContextBuilder
-          .keyManager(privateKeySupplier.get(), certificateSupplier.get());
-    }
-
-    SslHandler sslHandler = sslContextBuilder
-        .build()
-        .newHandler(channel.alloc(), host, protocol.port());
+            .trustManager(trustedCertificates)
+            .build()
+            .newHandler(channel.alloc(), protocol.host(), protocol.port());
 
     // Enable hostname verification.
     SSLEngine sslEngine = sslHandler.engine();
@@ -116,4 +84,3 @@ public class SslClientInitializer<C extends Channel> extends ChannelInitializer<
     channel.pipeline().addLast(sslHandler);
   }
 }
-
--- prober/src/test/java/google/registry/monitoring/blackbox/TestUtils.java
+++ prober/src/test/java/google/registry/monitoring/blackbox/TestUtils.java
@@ -1,4 +1,4 @@
-// Copyright 2019 The Nomulus Authors. All Rights Reserved.
+// Copyright 2017 The Nomulus Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,22 +14,39 @@
 
 package google.registry.monitoring.blackbox;
 
+import static com.google.common.truth.Truth.assertThat;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpMessage;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpMessage;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.cookie.ClientCookieEncoder;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 
-/**
- * Utility class for various helper methods used in testing.
- */
+/** Utility class for various helper methods used in testing. */
 public class TestUtils {
+
+  public static FullHttpRequest makeHttpPostRequest(String content, String host, String path) {
+    ByteBuf buf = Unpooled.wrappedBuffer(content.getBytes(US_ASCII));
+    FullHttpRequest request =
+        new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, path, buf);
+    request
+        .headers()
+        .set("user-agent", "Proxy")
+        .set("host", host)
+        .setInt("content-length", buf.readableBytes());
+    return request;
+  }
 
   public static FullHttpRequest makeHttpGetRequest(String host, String path) {
     FullHttpRequest request =
@@ -51,20 +68,80 @@ public class TestUtils {
     return response;
   }
 
-  /**
-   * Creates HttpResponse given status, redirection location, and other necessary inputs
-   */
-  public static FullHttpResponse makeRedirectResponse(
-      HttpResponseStatus status, String location, boolean keepAlive) {
-    FullHttpResponse response = makeHttpResponse("", status);
-    response.headers().set("content-type", "text/plain");
-    if (location != null) {
-      response.headers().set("location", location);
+  public static FullHttpRequest makeWhoisHttpRequest(
+      String content, String host, String path, String accessToken) {
+    FullHttpRequest request = makeHttpPostRequest(content, host, path);
+    request
+        .headers()
+        .set("authorization", "Bearer " + accessToken)
+        .set("content-type", "text/plain")
+        .set("accept", "text/plain");
+    return request;
+  }
+
+  public static FullHttpRequest makeEppHttpRequest(
+      String content,
+      String host,
+      String path,
+      String accessToken,
+      String sslClientCertificateHash,
+      String clientAddress,
+      Cookie... cookies) {
+    FullHttpRequest request = makeHttpPostRequest(content, host, path);
+    request
+        .headers()
+        .set("authorization", "Bearer " + accessToken)
+        .set("content-type", "application/epp+xml")
+        .set("accept", "application/epp+xml")
+        .set("X-SSL-Certificate", sslClientCertificateHash)
+        .set("X-Forwarded-For", clientAddress);
+    if (cookies.length != 0) {
+      request.headers().set("cookie", ClientCookieEncoder.STRICT.encode(cookies));
     }
-    if (keepAlive) {
-      response.headers().set("connection", "keep-alive");
+    return request;
+  }
+
+  public static FullHttpResponse makeWhoisHttpResponse(String content, HttpResponseStatus status) {
+    FullHttpResponse response = makeHttpResponse(content, status);
+    response.headers().set("content-type", "text/plain");
+    return response;
+  }
+
+  public static FullHttpResponse makeEppHttpResponse(
+      String content, HttpResponseStatus status, Cookie... cookies) {
+    FullHttpResponse response = makeHttpResponse(content, status);
+    response.headers().set("content-type", "application/epp+xml");
+    for (Cookie cookie : cookies) {
+      response.headers().add("set-cookie", ServerCookieEncoder.STRICT.encode(cookie));
     }
     return response;
   }
-}
 
+  /**
+   * Compares two {@link FullHttpMessage} for equivalency.
+   *
+   * <p>This method is needed because an HTTP message decoded and aggregated from inbound {@link
+   * ByteBuf} is of a different class than the one written to the outbound {@link ByteBuf}, and The
+   * {@link ByteBuf} implementations that hold the content of the HTTP messages are different, even
+   * though the actual content, headers, etc are the same.
+   *
+   * <p>This method is not type-safe, msg1 & msg2 can be a request and a response, respectively. Do
+   * not use this method directly.
+   */
+  private static void assertHttpMessageEquivalent(HttpMessage msg1, HttpMessage msg2) {
+    assertThat(msg1.protocolVersion()).isEqualTo(msg2.protocolVersion());
+    assertThat(msg1.headers()).isEqualTo(msg2.headers());
+    if (msg1 instanceof FullHttpRequest && msg2 instanceof FullHttpRequest) {
+      assertThat(((FullHttpRequest) msg1).content()).isEqualTo(((FullHttpRequest) msg2).content());
+    }
+  }
+
+  public static void assertHttpResponseEquivalent(FullHttpResponse res1, FullHttpResponse res2) {
+    assertThat(res1.status()).isEqualTo(res2.status());
+    assertHttpMessageEquivalent(res1, res2);
+  }
+
+  public static void assertHttpRequestEquivalent(HttpRequest req1, HttpRequest req2) {
+    assertHttpMessageEquivalent(req1, req2);
+  }
+}
