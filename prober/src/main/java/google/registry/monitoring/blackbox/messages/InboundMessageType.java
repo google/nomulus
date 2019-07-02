199137e04e5895ea7690906b87e185d9a880e084
Aman Sanger <sangera@google.com>
2019-07-02 10:58:52 -0400

    Fixed changes suggested by CydeWeys

--- prober/src/main/java/google/registry/monitoring/blackbox/messages/InboundMessageType.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/messages/InboundMessageType.java
@@ -12,10 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package google.registry.monitoring.blackbox.messages;
-
-/**
- * Marker Interface that is implemented by all classes that serve as {@code inboundMessages} in channel pipeline
- */
-public interface InboundMessageType {}
-
+package google.registry.monitoring.blackbox;
