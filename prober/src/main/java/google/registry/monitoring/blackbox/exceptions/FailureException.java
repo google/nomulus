4c00fb38795d3698f7319724cd040b32b0c2a4cb
Aman Sanger <sangera@google.com>
2019-07-02 10:58:52 -0400

    Fixed changes suggested by CydeWeys

--- prober/src/main/java/google/registry/monitoring/blackbox/exceptions/FailureException.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/exceptions/FailureException.java
@@ -12,18 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package google.registry.monitoring.blackbox.exceptions;
+package google.registry.monitoring.blackbox;
 
-/**
- * Base exception class for all instances when the status of the action performed is FAILURE.
- */
-public class FailureException extends Exception {
-
-  public FailureException(String msg) {
-    super(msg);
-  }
-
-  public FailureException(Throwable e) {
-    super(e);
-  }
-}
