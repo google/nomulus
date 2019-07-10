3b793c09125436b11fbba54ceb0f01db9b8a1d06
Aman Sanger <sangera@google.com>
2019-07-10 17:41:55 -0400

    Fully Updated ActionHandler (missing updated JavaDoc)

--- prober/src/main/java/google/registry/monitoring/blackbox/handlers/ActionHandler.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/handlers/ActionHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2017 The Nomulus Authors. All Rights Reserved.
+// Copyright 2019 The Nomulus Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,72 +15,58 @@
 package google.registry.monitoring.blackbox.handlers;
 
 import com.google.common.flogger.FluentLogger;
-import com.google.common.flogger.StackSize;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import google.registry.monitoring.blackbox.messages.InboundMarker;
+import google.registry.monitoring.blackbox.messages.OutboundMarker;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import io.netty.handler.codec.http.HttpResponse;
-import java.util.function.Function;
-import javax.inject.Inject;
 
 /**
  *
- * @param <I> Generic Type of Inbound Message
- * @param <O> Generic Type of Outbound Message
+ *
  * Abstract class that tells sends message down pipeline and
  * and tells listeners to move on when the message is received.
  */
-public abstract class ActionHandler<I, O> extends SimpleChannelInboundHandler<I>
-    implements Function<O, ChannelFuture> {
+public abstract class ActionHandler extends SimpleChannelInboundHandler<InboundMarker> {
 
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   protected ChannelPromise finished;
-  private Channel channel;
+  protected OutboundMarker outboundMessage;
 
+  /** Writes and flushes specified outboundMessage to channel pipeline and returns future
+   * that is marked as success when ActionHandler next reads from the channel */
+  public ChannelFuture getFuture(OutboundMarker outboundMessage) {
+    //Action Handlers subclasses require the outboundMessage for additional logic
+    this.outboundMessage = outboundMessage;
 
-  /** returns ChannelPromise for when inbound message is recieved
-   * a
-   * @param outboundMessage
-   * @return
-   */
-  @Override
-  public ChannelFuture apply(O outboundMessage) {
-    // Sends request along Outbound Handlers on the Pipeline
-
-    channel.writeAndFlush(outboundMessage);
+    //returns the ChannelPromise initialized
     return finished;
-
-  }
-
-  public void resetFuture() {
-    finished = channel.newPromise();
   }
 
   @Override
   public void handlerAdded(ChannelHandlerContext ctx) {
     //Once handler is added to channel pipeline, initialize channel and future for this handler
-
-    channel = ctx.channel();
     finished = ctx.newPromise();
   }
 
   @Override
-  public void channelRead0(ChannelHandlerContext ctx, I inboundMessage) throws Exception{
-    //Only purpose of Handler is to mark future as a success
-
+  public void channelRead0(ChannelHandlerContext ctx, InboundMarker inboundMessage) throws Exception {
+    //simply marks finished as success
     finished.setSuccess();
   }
 
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-    logger.atSevere().withCause(cause).withStackTrace(StackSize.FULL).log("Exception Caught");
+    logger.atSevere().withCause(cause).log(String.format(
+        "Attempted Action was unsuccessful with channel: %s, having pipeline: %s",
+        ctx.channel().toString(),
+        ctx.channel().pipeline().toString()));
+
+    finished.setFailure(cause);
+    ChannelFuture closedFuture = ctx.channel().close();
+    closedFuture.addListener(f -> logger.atInfo().log("Unsuccessful channel connection closed"));
   }
-
-
-
 }
 
