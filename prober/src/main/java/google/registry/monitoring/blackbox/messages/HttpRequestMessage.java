c5d3e069f5f195eb92ec2ecebc016e1c075c9720
Aman Sanger <sangera@google.com>
2019-07-16 11:03:40 -0400

    Rebased to Master and added in modified Handlers and ProbingAction

---
+++ prober/src/main/java/google/registry/monitoring/blackbox/messages/HttpRequestMessage.java
@@ -0,0 +1,40 @@
+package google.registry.monitoring.blackbox.messages;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+
+public class HttpRequestMessage extends DefaultFullHttpRequest implements OutboundMessageType {
+
+  public HttpRequestMessage(HttpVersion httpVersion, HttpMethod method, String uri) {
+    super(httpVersion, method, uri);
+  }
+  public HttpRequestMessage(HttpVersion httpVersion, HttpMethod method, String uri, ByteBuf content) {
+    super(httpVersion, method, uri, content);
+  }
+  public HttpRequestMessage(HttpVersion httpVersion, HttpMethod method, String uri, ByteBuf content, boolean validateHeaders) {
+    super(httpVersion, method, uri, content, validateHeaders);
+  }
+
+
+
+  @Override
+  public HttpRequestMessage setUri(String path) {
+    super.setUri(path);
+    return this;
+  }
+
+  public static HttpRequestMessage fromRequest(FullHttpRequest request) {
+    ByteBuf buf = request.content();
+
+    if (buf == null) {
+      return new HttpRequestMessage(HttpVersion.HTTP_1_1, request.method(), request.uri());
+    } else {
+     return new HttpRequestMessage(HttpVersion.HTTP_1_1, request.method(), request.uri(), buf);
+    }
+
+  }
+
+}
