60a04c931204f96f5c0dfad5164975d2408cc338
Aman Sanger <sangera@google.com>
2019-07-18 16:00:25 -0400

    fixed build issues

--- prober/src/test/java/google/registry/monitoring/blackbox/TestServers/WebWhoisServer.java
+++ prober/src/test/java/google/registry/monitoring/blackbox/TestServers/WebWhoisServer.java
@@ -1,3 +1,17 @@
+// Copyright 2019 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package google.registry.monitoring.blackbox.TestServers;
 
 import static google.registry.monitoring.blackbox.TestUtils.makeHttpResponse;
@@ -21,10 +35,10 @@ import io.netty.handler.codec.http.HttpServerCodec;
 /**
  * {@link TestServer} subtype that performs WebWhois Services Expected
  *
- * It will either redirect the client to the correct location if given the
+ * <p>It will either redirect the client to the correct location if given the
  * requisite redirect input, give the client a successful response if they give
  * the expected final destination, or give the client an error message if given
- * an unexpected host location
+ * an unexpected host location</p>
  */
 public class WebWhoisServer extends TestServer {
 
