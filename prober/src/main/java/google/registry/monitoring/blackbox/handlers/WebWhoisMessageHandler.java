6ecef9ce5a696ef985a123492830f01b8d1c497f
Aman Sanger <sangera@google.com>
2019-07-18 16:00:25 -0400

    fixed build issues

--- prober/src/main/java/google/registry/monitoring/blackbox/handlers/WebWhoisMessageHandler.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/handlers/WebWhoisMessageHandler.java
@@ -1,3 +1,17 @@
+// Copyright 2019 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package google.registry.monitoring.blackbox.handlers;
 
 import google.registry.monitoring.blackbox.messages.HttpRequestMessage;
@@ -8,6 +22,11 @@ import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.http.FullHttpResponse;
 import javax.inject.Inject;
 
+/**
+ * Subclass of {@link MessageHandler} that converts inbound {@link FullHttpResponse}
+ * to custom type {@link HttpResponseMessage} and retains {@link HttpRequestMessage}
+ * in case of reuse for redirection
+ */
 public class WebWhoisMessageHandler extends MessageHandler {
 
   private HttpRequestMessage request;
@@ -15,6 +34,7 @@ public class WebWhoisMessageHandler extends MessageHandler {
   @Inject
   public WebWhoisMessageHandler() {}
 
+  /** Retains {@link HttpRequestMessage} and calls super write method*/
   @Override
   public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
     request = (HttpRequestMessage) msg;
@@ -23,9 +43,9 @@ public class WebWhoisMessageHandler extends MessageHandler {
   }
 
 
+  /** Converts {@link FullHttpResponse} to {@link HttpResponseMessage}, so it is an {@link InboundMessageType} instance */
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
     FullHttpResponse originalResponse = (FullHttpResponse) msg;
     InboundMessageType response = HttpResponseMessage.fromResponse(originalResponse);
     super.channelRead(ctx, response);
