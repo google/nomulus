4a287c45b7e0dfc51dd16d174d1cf8cf6ab19bcc
Aman Sanger <sangera@google.com>
2019-07-18 16:00:25 -0400

    fixed build issues

--- prober/src/main/java/google/registry/monitoring/blackbox/ProbingStep.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/ProbingStep.java
@@ -16,14 +16,12 @@ package google.registry.monitoring.blackbox;
 
 import com.google.common.flogger.FluentLogger;
 import google.registry.monitoring.blackbox.Tokens.Token;
-import google.registry.monitoring.blackbox.exceptions.EppClientException;
 import google.registry.monitoring.blackbox.exceptions.InternalException;
-import google.registry.monitoring.blackbox.messages.HttpRequestMessage;
 import google.registry.monitoring.blackbox.messages.OutboundMessageType;
 import io.netty.channel.AbstractChannel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.local.LocalAddress;
-import java.io.IOException;
+
 import java.util.function.Consumer;
 import org.joda.time.Duration;
 
@@ -164,7 +162,4 @@ public abstract class ProbingStep<C extends AbstractChannel> implements Consumer
         message(),
         parent);
   }
-
 }
-
-
