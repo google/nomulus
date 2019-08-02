321fc7f94cad95a9bb772e779e0a2f1439116261
Aman Sanger <sangera@google.com>
2019-08-02 14:29:48 -0400

    Javadoc style fix in tests and removed unused methods

--- prober/src/test/java/google/registry/monitoring/blackbox/handlers/ConversionHandler.java
+++ prober/src/test/java/google/registry/monitoring/blackbox/handlers/ConversionHandler.java
@@ -18,9 +18,12 @@ import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import google.registry.monitoring.blackbox.TestUtils.DuplexMessageTest;
+import google.registry.monitoring.blackbox.messages.InboundMessageType;
+import google.registry.monitoring.blackbox.messages.OutboundMessageType;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 
@@ -28,7 +31,7 @@ import io.netty.channel.ChannelPromise;
  *  {@link ChannelHandler} used in tests to convert {@link OutboundMessageType} to
  *  to {@link ByteBuf}s and convert {@link ByteBuf}s to {@link InboundMessageType}
  *
- *  <p>Specific type of {@link OutboundMessage Type} and {@link InboundMessageType}
+ *  <p>Specific type of {@link OutboundMessageType} and {@link InboundMessageType}
  *  used for conversion is the {@link DuplexMessageTest} type.</p>
  */
 public class ConversionHandler extends ChannelDuplexHandler {
