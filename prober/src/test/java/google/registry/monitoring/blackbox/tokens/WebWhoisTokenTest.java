0e674a99fd12724eb29b45f7d32942cb5b5d418a
Aman Sanger <sangera@google.com>
2019-08-06 15:45:32 -0400

    Modified tests for WebWhois according to changes suggested by laijiang.

--- prober/src/test/java/google/registry/monitoring/blackbox/tokens/WebWhoisTokenTest.java
+++ prober/src/test/java/google/registry/monitoring/blackbox/tokens/WebWhoisTokenTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package google.registry.monitoring.blackbox;
+package google.registry.monitoring.blackbox.tokens;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -26,34 +26,45 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Unit Tests for each {@link Token} subtype (just {@link WebWhoisToken} for now)
+ * Unit Tests for {@link WebWhoisToken}
  */
 @RunWith(JUnit4.class)
-public class TokenTest {
+public class WebWhoisTokenTest {
 
   private static String PREFIX = "whois.nic.";
-  private static String TEST_STARTER = "starter";
-  private static ImmutableList<String> TEST_DOMAINS = ImmutableList.of("test");
+  private static String HOST = "starter";
+  private static String FIRST_TLD = "first_test";
+  private static String SECOND_TLD = "second_test";
+  private static String THIRD_TLD = "third_test";
+  private static ImmutableList<String> TEST_DOMAINS = ImmutableList.of(FIRST_TLD, SECOND_TLD,
+      THIRD_TLD);
 
   public Token webToken = new WebWhoisToken(TEST_DOMAINS);
 
   @Test
-  public void testWebToken_MessageModificationSuccess() {
+  public void testMessageModification() throws UndeterminedStateException {
     //creates Request message with header
     HttpRequestMessage message = new HttpRequestMessage();
-    message.headers().set("host", TEST_STARTER);
+    message.headers().set("host", HOST);
 
     //attempts to use Token's method for modifying the method based on its stored host
-    try {
-      HttpRequestMessage secondMessage = (HttpRequestMessage) webToken.modifyMessage(message);
-      assertThat(secondMessage.headers().get("host")).isEqualTo(PREFIX+TEST_DOMAINS.get(0));
-    } catch(UndeterminedStateException e) {
-      throw new RuntimeException(e);
-    }
+    HttpRequestMessage secondMessage = (HttpRequestMessage) webToken.modifyMessage(message);
+    assertThat(secondMessage.headers().get("host")).isEqualTo(PREFIX + TEST_DOMAINS.get(0));
+  }
 
+  /**
+   * As Circular Linked List has not been implemented yet, we cannot yet wrap around, so we don't
+   * test that in testing {@code next}.
+   */
+  @Test
+  public void testNextToken() {
+    assertThat(webToken.host()).isEqualTo(PREFIX + FIRST_TLD);
+    webToken = webToken.next();
 
+    assertThat(webToken.host()).isEqualTo(PREFIX + SECOND_TLD);
+    webToken = webToken.next();
 
-
+    assertThat(webToken.host()).isEqualTo(PREFIX + THIRD_TLD);
   }
 
 }
