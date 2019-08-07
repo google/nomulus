c055e0386a3e8453ede975456b8985b4f4ab1a0b
Aman Sanger <sangera@google.com>
2019-08-07 15:11:53 -0400

    Fixed warnings for java compilation

--- prober/src/main/java/google/registry/monitoring/blackbox/Protocol.java
+++ prober/src/main/java/google/registry/monitoring/blackbox/Protocol.java
@@ -58,7 +58,7 @@ public abstract class Protocol {
   }
 
   @Override
-  public String toString() {
+  public final String toString() {
     return String.format(
         "Protocol with name: %s, port: %d, providers: %s, and persistent connection: %s",
         name(),
