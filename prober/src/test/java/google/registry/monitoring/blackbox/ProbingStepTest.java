20b1d17e58475733f957939484a30430e4ed3633
Aman Sanger <sangera@google.com>
2019-08-07 09:57:34 -0400

    ProbingStepTest modified to have fewer unnecessary helper methods

--- prober/src/test/java/google/registry/monitoring/blackbox/ProbingStepTest.java
+++ prober/src/test/java/google/registry/monitoring/blackbox/ProbingStepTest.java
@@ -99,6 +99,7 @@ public class ProbingStepTest {
         .setProtocol(testProtocol)
         .build();
 
+
     ProbingAction testAction = testStep.generateAction(testToken);
 
     assertThat(testAction.channel()).isEqualTo(channel);
