58a0c18efd7352a08549f195c025f81cdf48bda0
Aman Sanger <sangera@google.com>
2019-08-08 16:00:49 -0400

    Fixed style issues in DefaultCircularLinkedListIterator and AbstractCircularLinkedListIterator

--- util/src/main/java/google/registry/util/DefaultCircularLinkedListIterator.java
+++ util/src/main/java/google/registry/util/DefaultCircularLinkedListIterator.java
@@ -15,30 +15,41 @@
 package google.registry.util;
 
 /**
- * Default implementation of {@link AbstractCircularLinkedListIterator} with generic type
- * {@param <T>} that operates only in closed circular loop.
+ * Default implementation of {@link AbstractCircularLinkedListIterator} with generic type {@param
+ * <T>} that operates only in closed circular loop.
  *
  * @param <T> - Type of elements in iterator.
- *
  */
 public class DefaultCircularLinkedListIterator<T> extends AbstractCircularLinkedListIterator<T> {
 
-  public static class Builder<T> extends AbstractCircularLinkedListIterator.Builder<T, Builder<T>, DefaultCircularLinkedListIterator<T>> {
+  private DefaultCircularLinkedListIterator(Entry<T> first, Entry<T> last) {
+    super(first, last);
+  }
+
+  /**
+   * {@link Builder} for {@link DefaultCircularLinkedListIterator}.
+   *
+   * <p>Doesn't do anything different than {@link AbstractCircularLinkedListIterator.Builder} but
+   * implementation of {@code childBuilder} and actual {@code build} methods.</p>
+   *
+   */
+  public static class Builder<T> extends
+      AbstractCircularLinkedListIterator.Builder<T, Builder<T>,
+          DefaultCircularLinkedListIterator<T>> {
 
     @Override
     public Builder<T> childBuilder() {
       return this;
     }
 
-    /** On build, close loop by pointing last element to first. */
+    /**
+     * On build, close loop by pointing last element to first.
+     */
     @Override
     public DefaultCircularLinkedListIterator<T> build() {
       current.setNext(first);
       return new DefaultCircularLinkedListIterator<>(first, current);
     }
-  }
-  private DefaultCircularLinkedListIterator(Entry<T> first, Entry<T> last) {
-    super(first, last);
   }
 
 }
