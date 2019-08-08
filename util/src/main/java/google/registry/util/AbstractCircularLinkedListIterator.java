303d850a9615a3c2c54c9edc4289afcc4b91673d
Aman Sanger <sangera@google.com>
2019-08-08 16:00:49 -0400

    Fixed style issues in DefaultCircularLinkedListIterator and AbstractCircularLinkedListIterator

--- util/src/main/java/google/registry/util/AbstractCircularLinkedListIterator.java
+++ util/src/main/java/google/registry/util/AbstractCircularLinkedListIterator.java
@@ -19,91 +19,133 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.util.Iterator;
 
 /**
- * Immutable class that support circular iteration through a group of elements of type {@param <T>}.
+ * Immutable class that support circular iteration through a group of elements of type {@param
+ * <T>}.
  *
  * @param <T> - Element type stored in the iterator
  *
  * <p>Is an immutable object that, when built, creates a circular pointing
- * group of Entries, that allows for looped iteration. The first and last
- * element in the closed loop are also stored.</p>
+ * group of Entries, that allows for looped iteration. The first and last element in the closed loop
+ * are also stored.</p>
  */
 public abstract class AbstractCircularLinkedListIterator<T> implements Iterator<T> {
 
-  /** First {@link Entry} in the circular iterator. */
+  /**
+   * First {@link Entry} in the circular iterator.
+   */
   private final Entry<T> first;
 
-  /** Last {@link Entry} in the circular iterator. */
+  /**
+   * Last {@link Entry} in the circular iterator.
+   */
   private final Entry<T> last;
 
-  /** Current {@link Entry} in iterator. */
+  /**
+   * Current {@link Entry} in iterator.
+   */
   private Entry<T> current;
 
-  /** Because this is a circular linked list iterator, there is always a next element. */
+  /**
+   * After having established pointers between {@link Entry}s, sets first element to the first
+   * {@link Entry} added to {@link Builder} and sets last element to last one added to {@link
+   * Builder}.
+   */
+  protected AbstractCircularLinkedListIterator(Entry<T> first, Entry<T> last) {
+    checkNotNull(last.next);
+    this.first = first;
+    this.last = last;
+  }
+
+  /**
+   * Because this is a circular linked list iterator, there is always a next element.
+   */
   @Override
   public boolean hasNext() {
     return true;
   }
 
-  /** Obtains the next element by calling on the {@link Entry}'s next element. */
+  /**
+   * Obtains the next element by calling on the {@link Entry}'s next element.
+   */
   @Override
   public T next() {
-    if (current == null)
-      //If this is first call of next, return first element
+    if (current == null) {
+    //If this is first call of next, return first element
+
       current = first;
-    else
-      //Otherwise return element after current element
+    } else {
+    //Otherwise return element after current element
+
       current = current.next;
+    }
 
     return current.data;
   }
 
-  /** Get method for current element in iterator. */
+  /**
+   * Get method for current element in iterator.
+   */
   public T get() {
-    if (current == null)
-      //if we have not started iterating, there is no current Entry, so return null
+    if (current == null) {
+    //if we have not started iterating, there is no current Entry, so return null
       return null;
-    else
-      //otherwise, retrieve the current Entry's data
+
+    } else {
+    //otherwise, retrieve the current Entry's data
       return current.data;
+    }
   }
 
-  /** Returns stored first element. */
+  /**
+   * Returns stored first element.
+   */
   public T getFirst() {
     return first.data;
   }
 
-  /** Returns stored last element. */
+  /**
+   * Returns stored last element.
+   */
   public T getLast() {
     return last.data;
   }
 
   /**
-   * Node class for {@link AbstractCircularLinkedListIterator} that stores value of element
-   * and points to next {@link Entry}.
+   * Node class for {@link AbstractCircularLinkedListIterator} that stores value of element and
+   * points to next {@link Entry}.
    *
    * @param <T> - Matching element type of iterator.
    */
   protected static class Entry<T> {
-    /** Stores the {@param <T>} instance. */
+
+    /**
+     * Stores the {@param <T>} instance.
+     */
     T data;
 
-    /** Saves reference to next {@link Entry} in iterator. */
+    /**
+     * Saves reference to next {@link Entry} in iterator.
+     */
     Entry<T> next;
 
-    /** Necessary for setting next {@link Entry} for subclasses outside of current package. */
-    public void setNext(Entry<T> next) {
-      this.next = next;
-    }
-
-    /** Only needs {@param <T>} instance for initialization. */
+    /**
+     * Only needs {@param <T>} instance for initialization.
+     */
     Entry(T data) {
       this.data = data;
+    }
+
+    /**
+     * Necessary for setting next {@link Entry} for subclasses outside of current package.
+     */
+    public void setNext(Entry<T> next) {
+      this.next = next;
     }
   }
 
   /**
-   * As {@link AbstractCircularLinkedListIterator} is an immutable class, it needs
-   * a builder for instantiation.
+   * As {@link AbstractCircularLinkedListIterator} is an immutable class, it needs a builder for
+   * instantiation.
    *
    * @param <T> - Matching element type of iterator
    *
@@ -113,61 +155,66 @@ public abstract class AbstractCircularLinkedListIterator<T> implements Iterator<
    * <p>Sets first element added to {@code first}, and when built, sets last added
    * element to {@code last} and points it to the {@code first} element.</p>
    */
-  public abstract static class Builder<T, B extends Builder<T, B, L>, L extends AbstractCircularLinkedListIterator> {
-    /** Matching first entry in the circular iterator to be built. */
+  public abstract static class Builder<T, B extends Builder<T, B, L>,
+      L extends AbstractCircularLinkedListIterator> {
+
+    /**
+     * Matching first entry in the circular iterator to be built.
+     */
     protected Entry<T> first;
 
-    /** {@link Entry} corresponding to most recent element added. */
+    /**
+     * {@link Entry} corresponding to most recent element added.
+     */
     protected Entry<T> current;
 
-    /** Sets current {@link Entry} to element added and points previous {@link Entry} to this one. */
+    /**
+     * Sets current {@link Entry} to element added and points previous {@link Entry} to this one.
+     */
     public Builder<T, B, L> addElement(T element) {
       Entry<T> nextEntry = new Entry<>(element);
-      if (current == null)
-        //If this is first element added, we set it to first
+      if (current == null) {
+      //If this is first element added, we set it to first
         first = nextEntry;
-      else
-        //Otherwise point previous Entry to this one
+      } else {
+      //Otherwise point previous Entry to this one
         current.next = nextEntry;
+      }
 
       current = nextEntry;
       return this;
     }
 
-    /** Simply calls {@code addElement}, for each element in {@code elements}. */
+    /**
+     * Simply calls {@code addElement}, for each element in {@code elements}.
+     */
     public Builder<T, B, L> addElements(Iterable<T> elements) {
       elements.forEach(this::addElement);
       return this;
     }
 
-    /** Simply calls {@code addElement}, for each element in {@code elements}. */
+    /**
+     * Simply calls {@code addElement}, for each element in {@code elements}.
+     */
     public Builder<T, B, L> addElements(T... elements) {
-      for (T element : elements)
+      for (T element : elements) {
         addElement(element);
+      }
 
       return this;
     }
 
     /**
-     *  Converts {@link Builder} to input subclass.
+     * Converts {@link Builder} to input subclass.
      *
      * <p>Necessary for applying custom build methods when constructing
      * subclasses that are inherent to the attributes of those subclasses.</p>
      */
     public abstract B childBuilder();
 
-    /** Points last {@link Entry} to first {@link Entry}, and calls private constructor. */
+    /**
+     * Points last {@link Entry} to first {@link Entry}, and calls private constructor.
+     */
     public abstract L build();
-  }
-
-  /**
-   * After having established pointers between {@link Entry}s, sets
-   * first element to the first {@link Entry} added to {@link Builder}
-   * and sets last element to last one added to {@link Builder}.
-   */
-  protected AbstractCircularLinkedListIterator(Entry<T> first, Entry<T> last) {
-      checkNotNull(last.next);
-      this.first = first;
-      this.last = last;
   }
 }
