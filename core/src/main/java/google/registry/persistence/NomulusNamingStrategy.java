303a3bea6effb80f43fe03c62e7917aff159ae45
Gus Brodman <gbrodman@google.com>
2019-09-06 16:14:12 -0400

    checkstyle

--- core/src/main/java/google/registry/persistence/NomulusNamingStrategy.java
+++ core/src/main/java/google/registry/persistence/NomulusNamingStrategy.java
@@ -71,6 +71,7 @@ public class NomulusNamingStrategy implements PhysicalNamingStrategy {
     return jdbcEnvironment
         .getIdentifierHelper()
         .toIdentifier(
-            CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, name.getText()), /* quoted= */ false);
+            CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, name.getText()),
+            /* quoted= */ false);
   }
 }
