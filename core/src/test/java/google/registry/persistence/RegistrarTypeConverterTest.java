70359d94b23bd46893b5bc0f3a65b8540df5b735
Shicong Huang <shicong@google.com>
2020-01-21 17:57:45 -0500

    Use 3 columns for address and use text for enum

--- core/src/test/java/google/registry/persistence/RegistrarTypeConverterTest.java
+++ core/src/test/java/google/registry/persistence/RegistrarTypeConverterTest.java
@@ -17,44 +17,47 @@ package google.registry.persistence;
 import static com.google.common.truth.Truth.assertThat;
 import static google.registry.model.transaction.TransactionManagerFactory.jpaTm;
 
-import com.google.common.collect.ImmutableMap;
 import google.registry.model.ImmutableObject;
-import google.registry.model.registrar.Registrar.BillingAccountEntry;
+import google.registry.model.registrar.Registrar.Type;
 import google.registry.model.transaction.JpaTestRules;
 import google.registry.model.transaction.JpaTestRules.JpaUnitTestRule;
-import java.util.Map;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import org.hibernate.annotations.Type;
-import org.joda.money.CurrencyUnit;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Unit tests for {@link CurrencyToBillingMapUserType}. */
+/** Unit tests for {@link RegistrarTypeConverter}. */
 @RunWith(JUnit4.class)
-public class CurrencyToBillingMapUserTypeTest {
+public class RegistrarTypeConverterTest {
   @Rule
   public final JpaUnitTestRule jpaRule =
-      new JpaTestRules.Builder()
-          .withInitScript("sql/flyway/V14__load_extension_for_hstore.sql")
-          .withEntityClass(TestEntity.class)
-          .buildUnitTestRule();
+      new JpaTestRules.Builder().withEntityClass(TestEntity.class).buildUnitTestRule();
 
   @Test
-  public void roundTripConversion_returnsSameCurrencyToBillingMap() {
-    ImmutableMap<CurrencyUnit, BillingAccountEntry> currencyToBilling =
-        ImmutableMap.of(
-            CurrencyUnit.of("USD"),
-            new BillingAccountEntry(CurrencyUnit.of("USD"), "accountId1"),
-            CurrencyUnit.of("CNY"),
-            new BillingAccountEntry(CurrencyUnit.of("CNY"), "accountId2"));
-    TestEntity testEntity = new TestEntity(currencyToBilling);
+  public void roundTripConversion_returnsSameEnum() {
+    TestEntity testEntity = new TestEntity(Type.MONITORING);
     jpaTm().transact(() -> jpaTm().getEntityManager().persist(testEntity));
     TestEntity persisted =
         jpaTm().transact(() -> jpaTm().getEntityManager().find(TestEntity.class, "id"));
-    assertThat(persisted.currencyToBilling).containsExactlyEntriesIn(currencyToBilling);
+    assertThat(persisted.type).isEqualTo(Type.MONITORING);
+  }
+
+  @Test
+  public void testNativeQuery_succeeds() {
+    TestEntity testEntity = new TestEntity(Type.MONITORING);
+    jpaTm().transact(() -> jpaTm().getEntityManager().persist(testEntity));
+
+    assertThat(
+            jpaTm()
+                .transact(
+                    () ->
+                        jpaTm()
+                            .getEntityManager()
+                            .createNativeQuery("SELECT type FROM \"TestEntity\" WHERE name = 'id'")
+                            .getSingleResult()))
+        .isEqualTo("MONITORING");
   }
 
   @Entity(name = "TestEntity") // Override entity name to avoid the nested class reference.
@@ -62,13 +65,12 @@ public class CurrencyToBillingMapUserTypeTest {
 
     @Id String name = "id";
 
-    @Type(type = "google.registry.persistence.CurrencyToBillingMapUserType")
-    Map<CurrencyUnit, BillingAccountEntry> currencyToBilling;
+    Type type;
 
     private TestEntity() {}
 
-    private TestEntity(Map<CurrencyUnit, BillingAccountEntry> currencyToBilling) {
-      this.currencyToBilling = currencyToBilling;
+    private TestEntity(Type type) {
+      this.type = type;
     }
   }
 }
