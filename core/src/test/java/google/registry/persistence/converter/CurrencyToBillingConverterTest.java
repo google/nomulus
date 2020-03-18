29cf4cdaf7350961a95035aa9a1da315d069d59d
Shicong Huang <shicong@google.com>
2020-03-18 15:29:57 -0400

    Delete old map usertype

--- core/src/test/java/google/registry/persistence/converter/CurrencyToBillingConverterTest.java
+++ core/src/test/java/google/registry/persistence/converter/CurrencyToBillingConverterTest.java
@@ -31,7 +31,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Unit tests for {@link CurrencyToBillingMapUserType}. */
+/** Unit tests for {@link CurrencyToBillingConverter}. */
 @RunWith(JUnit4.class)
 public class CurrencyToBillingConverterTest {
   @Rule
