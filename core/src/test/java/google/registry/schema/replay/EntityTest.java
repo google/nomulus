a04a89c6262bceac7fe5c16c152b5ee294beb958
Gus Brodman <gbrodman@google.com>
2020-04-21 15:43:54 -0400

    Add 1-1 mapping between entity annotations and interfaces

--- core/src/test/java/google/registry/schema/replay/EntityTest.java
+++ core/src/test/java/google/registry/schema/replay/EntityTest.java
@@ -14,9 +14,12 @@
 
 package google.registry.schema.replay;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.collect.ImmutableSet;
 import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ClassInfoList;
 import io.github.classgraph.ScanResult;
 import org.junit.Ignore;
@@ -32,18 +35,31 @@ public class EntityTest {
   public void testSqlEntityPersistence() {
     try (ScanResult scanResult =
         new ClassGraph().enableAnnotationInfo().whitelistPackages("google.registry").scan()) {
-      // All javax.persistence entities must implement SqlEntity
-      ClassInfoList failingJavaxEntities =
-          scanResult
-              .getClassesWithAnnotation(javax.persistence.Entity.class.getName())
-              .filter(info -> !info.implementsInterface(SqlEntity.class.getName()));
-      assertThat(failingJavaxEntities).isEmpty();
-      // All com.googlecode.objectify.annotation.Entity classes must implement DatastoreEntity
-      ClassInfoList failingObjectifyEntries =
-          scanResult
-              .getClassesWithAnnotation(com.googlecode.objectify.annotation.Entity.class.getName())
-              .filter(info -> !info.implementsInterface(DatastoreEntity.class.getName()));
-      assertThat(failingObjectifyEntries).isEmpty();
+      // All javax.persistence entities must implement SqlEntity and vice versa
+      ImmutableSet<String> javaxPersistenceClasses =
+          getClassNames(
+              scanResult.getClassesWithAnnotation(javax.persistence.Entity.class.getName()));
+      ImmutableSet<String> sqlEntityClasses =
+          getClassNames(scanResult.getClassesImplementing(SqlEntity.class.getName()));
+      assertThat(javaxPersistenceClasses).isEqualTo(sqlEntityClasses);
+
+      // All com.googlecode.objectify.annotation.Entity classes must implement DatastoreEntity and
+      // vice versa
+      ImmutableSet<String> objectifyClasses =
+          getClassNames(
+              scanResult.getClassesWithAnnotation(
+                  com.googlecode.objectify.annotation.Entity.class.getName()));
+      ImmutableSet<String> datastoreEntityClasses =
+          getClassNames(scanResult.getClassesImplementing(DatastoreEntity.class.getName()));
+      assertThat(objectifyClasses).isEqualTo(datastoreEntityClasses);
     }
+  }
+
+  private ImmutableSet<String> getClassNames(ClassInfoList classInfoList) {
+    return classInfoList.stream()
+        .filter(ClassInfo::isStandardClass)
+        .map(ClassInfo::loadClass)
+        .map(Class::getName)
+        .collect(toImmutableSet());
   }
 }
