faaf0a3130a8cab8b502398b13a4a32890cf0067
Ben McIlwain <mcilwain@google.com>
2020-04-24 12:48:21 -0400

    Try AppEngineRule

--- core/src/test/java/google/registry/tools/RegistryToolTest.java
+++ core/src/test/java/google/registry/tools/RegistryToolTest.java
@@ -18,15 +18,20 @@ import static com.google.common.truth.Truth.assertThat;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertThrows;
 
+import google.registry.testing.AppEngineRule;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.concurrent.locks.ReentrantLock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 /** Unit tests for {@link google.registry.tools.RegistryTool}. */
 public class RegistryToolTest {
+
+  @RegisterExtension
+  public final AppEngineRule appEngine = AppEngineRule.builder().withDatastoreAndCloudSql().build();
 
   // Lock for stdout/stderr.  Note that this is static: since we're dealing with globals, we need
   // to lock for the entire JVM.
