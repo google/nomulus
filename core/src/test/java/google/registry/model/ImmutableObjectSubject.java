4dc3762e325bb11f109bd1b366ac94d559347e22
Gus Brodman <gbrodman@google.com>
2020-07-17 11:53:47 -0400

    Create an ImmutableObject Correspondence for more flexible usage

--- core/src/test/java/google/registry/model/ImmutableObjectSubject.java
+++ core/src/test/java/google/registry/model/ImmutableObjectSubject.java
@@ -18,12 +18,16 @@ import static com.google.common.truth.Truth.assertAbout;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.truth.Correspondence;
+import com.google.common.truth.Correspondence.BinaryPredicate;
 import com.google.common.truth.FailureMetadata;
 import com.google.common.truth.SimpleSubjectBuilder;
 import com.google.common.truth.Subject;
 import java.lang.reflect.Field;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
+import javax.annotation.Nullable;
 
 /** Truth subject for asserting things about ImmutableObjects that are not built in. */
 public final class ImmutableObjectSubject extends Subject {
@@ -41,16 +45,44 @@ public final class ImmutableObjectSubject extends Subject {
     assertThat(actualFields).containsExactlyEntriesIn(expectedFields);
   }
 
+  public static Correspondence<ImmutableObject, ImmutableObject> immutableObjectCorrespondence(
+      String... ignoredFields) {
+    return Correspondence.from(
+        new ImmutableObjectBinaryPredicate(ignoredFields), "has all relevant fields equal to");
+  }
+
   public static SimpleSubjectBuilder<ImmutableObjectSubject, ImmutableObject>
       assertAboutImmutableObjects() {
     return assertAbout(ImmutableObjectSubject::new);
   }
 
-  private static Map<Field, Object> filterFields(ImmutableObject original,
-      String... ignoredFields) {
-    Map<Field, Object> originalFields = ModelUtils.getFieldValues(original);
-    ImmutableSet<String> ignoredFieldSet = ImmutableSet.copyOf(ignoredFields);
+  private static class ImmutableObjectBinaryPredicate
+      implements BinaryPredicate<ImmutableObject, ImmutableObject> {
 
+    private final String[] ignoredFields;
+
+    private ImmutableObjectBinaryPredicate(String... ignoredFields) {
+      this.ignoredFields = ignoredFields;
+    }
+
+    @Override
+    public boolean apply(@Nullable ImmutableObject actual, @Nullable ImmutableObject expected) {
+      if (actual == null && expected == null) {
+        return true;
+      }
+      if (actual == null || expected == null) {
+        return false;
+      }
+      Map<Field, Object> actualFields = filterFields(actual, ignoredFields);
+      Map<Field, Object> expectedFields = filterFields(expected, ignoredFields);
+      return Objects.equals(actualFields, expectedFields);
+    }
+  }
+
+  private static Map<Field, Object> filterFields(
+      ImmutableObject original, String... ignoredFields) {
+    ImmutableSet<String> ignoredFieldSet = ImmutableSet.copyOf(ignoredFields);
+    Map<Field, Object> originalFields = ModelUtils.getFieldValues(original);
     // don't use ImmutableMap or a stream->collect model since we can have nulls
     Map<Field, Object> result = new LinkedHashMap<>();
     for (Map.Entry<Field, Object> entry : originalFields.entrySet()) {
