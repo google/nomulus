85734ef595763053386851eb38f4d6ca1cc6b33b
Gus Brodman <gbrodman@google.com>
2020-07-22 14:51:57 -0400

    Actually we don't need this for remote deployment

--- core/src/main/java/google/registry/beam/initsql/BeamJpaModule.java
+++ core/src/main/java/google/registry/beam/initsql/BeamJpaModule.java
@@ -42,7 +42,6 @@ import javax.annotation.Nullable;
 import javax.inject.Singleton;
 import org.apache.beam.sdk.io.FileSystems;
 import org.apache.beam.sdk.io.fs.ResourceId;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
 /**
  * Provides bindings for {@link JpaTransactionManager} to Cloud SQL.
@@ -101,7 +100,6 @@ public class BeamJpaModule {
   }
 
   String readOnlyLineFromCredentialFile() {
-    FileSystems.setDefaultPipelineOptions(PipelineOptionsFactory.create());
     try {
       ResourceId resourceId = FileSystems.matchSingleFileSpec(credentialFilePath).resourceId();
       try (BufferedReader reader =
