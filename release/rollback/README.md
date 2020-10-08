2c4174fabd477c0ec7bf1253fe53cdb8cf3fcf74
Weimin Yu <weiminyu@google.com>
2020-10-07 21:45:15 -0400

    An automated rollback tool for Nomulus
    A tool that directs traffic between deployed versions. It handles the
    conversion between Nomulus tags and AppEngine versions, executes schema
    compatibility tests, ensures that steps are executed in the correct order,
    and updates deployment records appropriately.

---
+++ release/rollback/README.md
@@ -0,0 +1,98 @@
+## Summary
+
+This package contains an automated rollback tool for the Nomulus server on
+AppEngine. When given the Nomulus tag of a deployed release, the tool directs
+all traffics in the four recognized services (backend, default, pubapi, and
+tools) to that release. In the process, it handles Nomulus tag to AppEngine
+version ID translation, checks the target binary's compatibility with SQL
+schema, starts/stops versions and redirects traffic in proper sequence, and
+updates deployment metadata appropriately.
+
+The tool has two limitations:
+
+1.  This tool only accepts one release tag as rollback target, which is applied
+    to all services.
+2.  The tool immediately migrates all traffic to the new versions. It does not
+    support gradual migration. This is not an issue now since gradual migration
+    is only available in automatically scaled versions, while none of versions
+    is using automatic scaling.
+
+Although this tool is named a rollback tool, it can also reverse a rollback,
+that is, rolling forward to a newer release.
+
+## Prerequisites
+
+This tool requires python version 3.7+. It also requires two GCP client
+libraries: google-cloud-storage and google-api-python-client.
+
+## Usage
+
+The tool can be invoked using the rollback_tool script in the Nomulus root
+directory. The following parameters may be requested:
+
+*   dev_project: This is the GCP project that hosts the release and deployment
+    infrastructure, including the Spinnaker pipelines.
+*   project: This is the GCP project that hosts the Nomulus server to be rolled
+    back.
+*   env: This is the name of the Nomulus environment, e.g., sandbox or
+    production. Although the project to environment is available in Gradle
+    scripts and internal configuration files, it is not easy to extract them.
+    Therefore, we require the user to provide it for now.
+
+A typical workflow goes as follows:
+
+### Check Which Release is Serving
+
+```shell
+rollback show_serving_release --dev_project ... --project ... --env ...
+```
+
+The output may look like:
+
+```
+backend nomulus-v049    nomulus-20201019-RC00
+default nomulus-v049    nomulus-20201019-RC00
+pubapi  nomulus-v049    nomulus-20201019-RC00
+tools   nomulus-v049    nomulus-20201019-RC00
+```
+
+### Review Recent Deployments
+
+```shell
+rollback show_recent_deployments --dev_project ... --project ... --env ...
+```
+
+This command displays up to 3 most recent deployments. The output (from sandbox
+which only has two tracked deployments as of the writing of this document) may
+look like:
+
+```
+backend nomulus-v048    nomulus-20201012-RC00
+default nomulus-v048    nomulus-20201012-RC00
+pubapi  nomulus-v048    nomulus-20201012-RC00
+tools   nomulus-v048    nomulus-20201012-RC00
+backend nomulus-v049    nomulus-20201019-RC00
+default nomulus-v049    nomulus-20201019-RC00
+pubapi  nomulus-v049    nomulus-20201019-RC00
+tools   nomulus-v049    nomulus-20201019-RC00
+```
+
+### Roll to the Target Release
+
+```shell
+rollback rollback --dev_project ... --project ... --env ... \
+    --targt_release {YOUR_CHOSEN_TAG} --run_mode ...
+```
+
+The rollback subcommand has two new parameters:
+
+*   target_release: This is the Nomulus tag of the target release, in the form
+    of nomulus-YYYYMMDD-RC[0-9][0-9]
+*   run_mode: This is the execution mode of the rollback action. There are three
+    modes:
+    1.  dryrun: The tool will only output information about every step of the
+        rollback, including commands that a user can copy and run elsewhere.
+    2.  interactive: The tool will prompt the user before executing each step.
+        The user may choose to abort the rollback, skip the step, or continue
+        with the step.
+    3.  automatic: Tool will execute all steps in one shot.
---
+++ release/rollback/appengine_test.py
@@ -0,0 +1,132 @@
+# Copyright 2020 The Nomulus Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Unit tests for appengine."""
+from typing import Any, Dict, List, Tuple, Union
+import unittest
+from unittest import mock
+from unittest.mock import patch
+
+import appengine
+import common
+
+
+def setup_appengine_admin() -> Tuple[object, object]:
+    """Helper for setting up a mocked AppEngineAdmin instance.
+
+    Returns:
+        An AppEngineAdmin instance and a request with which API responses can
+        be mocked.
+    """
+
+    # Assign mocked API response to mock_request.execute.
+    mock_request = mock.Mock('googleapiclient.http.HttpRequest')
+    mock_request.uri = mock.Mock(return_value='myuri')
+    # Mocked resource shared by services, versions, and operations.
+    resource = mock.Mock(object)
+    resource.list = mock.Mock(return_value=mock_request)
+    resource.get = mock.Mock(return_value=mock_request)
+    resource.patch = mock.Mock(return_value=mock_request)
+
+    # Root resource of AppEngine API. Exact type unknown.
+    apps = mock.Mock(object)
+    apps.services = mock.Mock(return_value=resource)
+    apps.services.return_value.versions = mock.Mock(return_value=resource)
+    apps.operations = mock.Mock(return_value=resource)
+    service_lookup = mock.Mock('discovery.Resource')
+    service_lookup.apps = mock.Mock(return_value=apps)
+    appengine_admin = appengine.AppEngineAdmin('project', service_lookup, 1)
+
+    return (appengine_admin, mock_request)
+
+
+class AppEngineTestCase(unittest.TestCase):
+    """Unit tests for appengine."""
+    def setUp(self) -> None:
+        self._client, self._mock_request = setup_appengine_admin()
+        self.addCleanup(patch.stopall)
+
+    def _set_mocked_response(
+            self, responses: Union[Dict[str, Any], List[Dict[str,
+                                                             Any]]]) -> None:
+        if isinstance(responses, list):
+            self._mock_request.execute = mock.Mock(side_effect=responses)
+        else:
+            self._mock_request.execute = mock.Mock(return_value=responses)
+
+    def test_checked_request_multipage_raises(self) -> None:
+        self._set_mocked_response({'nextPageToken': ''})
+        self.assertRaises(appengine.PagingError,
+                          self._client.get_serving_versions)
+
+    def test_get_serving_versions(self) -> None:
+        self._set_mocked_response({
+            'services': [{
+                'split': {
+                    'allocations': {
+                        'my_version': 3.14,
+                    }
+                },
+                'id': 'pubapi'
+            }, {
+                'split': {
+                    'allocations': {
+                        'another_version': 2.71,
+                    }
+                },
+                'id': 'error_dashboard'
+            }]
+        })
+        self.assertEqual(
+            self._client.get_serving_versions(),
+            frozenset([common.VersionKey('pubapi', 'my_version')]))
+
+    def test_get_version_configs(self):
+        self._set_mocked_response({
+            'versions': [{
+                'basicScaling': {
+                    'maxInstances': 10
+                },
+                'id': 'version'
+            }]
+        })
+        self.assertEqual(
+            self._client.get_version_configs(
+                frozenset([common.VersionKey('default', 'version')])),
+            frozenset([
+                common.VersionConfig('default', 'version',
+                                     common.AppEngineScaling.BASIC)
+            ]))
+
+    def test_async_update(self):
+        self._set_mocked_response([
+            {
+                'name': 'project/operations/op_id',
+                'done': False
+            },
+            {
+                'name': 'project/operations/op_id',
+                'done': False
+            },
+            {
+                'name': 'project/operations/op_id',
+                'response': {},
+                'done': True
+            },
+        ])
+        self._client.set_manual_scaling_num_instance('service', 'version', 1)
+        self.assertEqual(self._mock_request.execute.call_count, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
