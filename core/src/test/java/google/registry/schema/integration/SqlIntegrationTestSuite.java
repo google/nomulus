2976666843d89ad52418e985be0584cbb00f54bc
Gus Brodman <gbrodman@google.com>
2020-10-15 16:13:21 -0400

    Add a SQL schema and DAO for KmsSecretRevision
    The dual-object nature of KmsSecret and KmsSecretRevision will not be
    necessary once we have moved to SQL. In that world, the only object will
    be the one now called KmsSecretRevision. KmsSecretRevision already
    stores its parent so all we need to do is convert that key to the String
    secretName (or from the secretName to the key, if loading from SQL) and
    select the max revision ID for a given secret name.
    
    In a future PR, we will add a dual-writing DAO to these objects and
    perform the dual writes, similar to how ReservedList functions.

--- core/src/test/java/google/registry/schema/integration/SqlIntegrationTestSuite.java
+++ core/src/test/java/google/registry/schema/integration/SqlIntegrationTestSuite.java
@@ -28,6 +28,7 @@ import google.registry.model.registry.RegistryLockDaoTest;
 import google.registry.model.registry.RegistryTest;
 import google.registry.model.registry.label.ReservedListSqlDaoTest;
 import google.registry.model.reporting.Spec11ThreatMatchTest;
+import google.registry.model.server.KmsSecretRevisionSqlDaoTest;
 import google.registry.model.tmch.ClaimsListDaoTest;
 import google.registry.persistence.transaction.JpaEntityCoverageExtension;
 import google.registry.persistence.transaction.JpaTestRules.JpaIntegrationWithCoverageExtension;
@@ -83,6 +84,7 @@ import org.junit.runner.RunWith;
   DomainBaseSqlTest.class,
   DomainHistoryTest.class,
   HostHistoryTest.class,
+  KmsSecretRevisionSqlDaoTest.class,
   LockDaoTest.class,
   PollMessageTest.class,
   PremiumListDaoTest.class,
