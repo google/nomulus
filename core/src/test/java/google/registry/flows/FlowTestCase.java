4a971b827ecff4312c72e80b0a3740aa0bfd1cca
Gus Brodman <gbrodman@google.com>
2021-04-13 12:09:08 -0400

    Update various tests to work with SQL as well
    The main weird bit here is adding a method in DatabaseHelper to
    retrieve and initialize all objects in either database. The
    initialization is necessary since it's used post-command-dry-run to make
    sure that no changes were actually made.

--- core/src/test/java/google/registry/flows/FlowTestCase.java
+++ core/src/test/java/google/registry/flows/FlowTestCase.java
@@ -24,6 +24,8 @@ import static google.registry.model.ofy.ObjectifyService.ofy;
 import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
 import static google.registry.persistence.transaction.TransactionManagerUtil.transactIfJpaTm;
 import static google.registry.testing.DatabaseHelper.stripBillingEventId;
+import static google.registry.util.DateTimeUtils.END_OF_TIME;
+import static google.registry.util.DateTimeUtils.START_OF_TIME;
 import static google.registry.xml.XmlTestUtils.assertXmlEquals;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.joda.time.DateTimeZone.UTC;
@@ -42,9 +44,11 @@ import google.registry.model.eppinput.EppInput;
 import google.registry.model.eppoutput.EppOutput;
 import google.registry.model.ofy.Ofy;
 import google.registry.model.reporting.HistoryEntry;
+import google.registry.model.reporting.HistoryEntryDao;
 import google.registry.model.tmch.ClaimsListShard.ClaimsListSingleton;
 import google.registry.monitoring.whitebox.EppMetric;
 import google.registry.testing.AppEngineExtension;
+import google.registry.testing.DatabaseHelper;
 import google.registry.testing.EppLoader;
 import google.registry.testing.FakeClock;
 import google.registry.testing.FakeHttpSession;
@@ -112,8 +116,8 @@ public abstract class FlowTestCase<F extends Flow> {
     sessionMetadata = new HttpSessionMetadata(new FakeHttpSession());
     sessionMetadata.setClientId("TheRegistrar");
     sessionMetadata.setServiceExtensionUris(ProtocolDefinition.getVisibleServiceExtensionUris());
-    ofy().saveWithoutBackup().entity(new ClaimsListSingleton()).now();
- }
+    tm().insertWithoutBackup(new ClaimsListSingleton());
+  }
 
   protected void removeServiceExtensionUri(String uri) {
     sessionMetadata.setServiceExtensionUris(
@@ -174,7 +178,11 @@ public abstract class FlowTestCase<F extends Flow> {
   }
 
   protected void assertNoHistory() {
-    assertThat(ofy().load().type(HistoryEntry.class)).isEmpty();
+    if (tm().isOfy()) {
+      assertThat(ofy().load().type(HistoryEntry.class)).isEmpty();
+    } else {
+      assertThat(HistoryEntryDao.loadAllHistoryObjects(START_OF_TIME, END_OF_TIME)).isEmpty();
+    }
   }
 
   /**
@@ -303,10 +311,10 @@ public abstract class FlowTestCase<F extends Flow> {
   private TmchCaMode tmchCaMode = TmchCaMode.PILOT;
 
   public EppOutput dryRunFlowAssertResponse(String xml, String... ignoredPaths) throws Exception {
-    List<Object> beforeEntities = ofy().load().list();
+    List<Object> beforeEntities = DatabaseHelper.loadAllEntities();
     EppOutput output =
         runFlowAssertResponse(CommitMode.DRY_RUN, UserPrivileges.NORMAL, xml, ignoredPaths);
-    assertThat(ofy().load()).containsExactlyElementsIn(beforeEntities);
+    assertThat(DatabaseHelper.loadAllEntities()).containsExactlyElementsIn(beforeEntities);
     return output;
   }
 
--- core/src/test/java/google/registry/testing/DatabaseHelper.java
+++ core/src/test/java/google/registry/testing/DatabaseHelper.java
@@ -116,6 +116,7 @@ import google.registry.model.transfer.TransferData;
 import google.registry.model.transfer.TransferStatus;
 import google.registry.persistence.VKey;
 import google.registry.tmch.LordnTaskUtils;
+import java.lang.reflect.Field;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -124,6 +125,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import javax.annotation.Nullable;
+import org.hibernate.Hibernate;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.joda.time.DateTime;
@@ -339,8 +341,7 @@ public class DatabaseHelper {
   /** Persists a domain and enqueues a LORDN task of the appropriate type for it. */
   public static DomainBase persistDomainAndEnqueueLordn(final DomainBase domain) {
     final DomainBase persistedDomain = persistResource(domain);
-    // Calls {@link LordnTaskUtils#enqueueDomainBaseTask} wrapped in an ofy transaction so that
-    // the
+    // Calls {@link LordnTaskUtils#enqueueDomainBaseTask} wrapped in a transaction so that the
     // transaction time is set correctly.
     tm().transactNew(() -> LordnTaskUtils.enqueueDomainBaseTask(persistedDomain));
     maybeAdvanceClock();
@@ -995,8 +996,8 @@ public class DatabaseHelper {
    * Persists a test resource to Datastore and returns it.
    *
    * <p>Tests should always use this method (or the shortcut persist methods in this class) to
-   * persist test data, to avoid potentially subtle bugs related to race conditions and a stale
-   * ofy() session cache. Specifically, this method calls .now() on the save to force the write to
+   * persist test data, to avoid potentially subtle bugs related to race conditions and a stale ofy
+   * session cache. Specifically, this method calls .now() on the save to force the write to
    * actually get sent to Datastore (although it does not force it to be applied) and clears the
    * session cache. If necessary, this method also updates the relevant {@link EppResourceIndex},
    * {@link ForeignKeyIndex}.
@@ -1279,6 +1280,45 @@ public class DatabaseHelper {
         Registrar.loadByClientId(clientId),
         "Error in tests: Registrar %s does not exist",
         clientId);
+  }
+
+  public static List<Object> loadAllEntities() {
+    if (tm().isOfy()) {
+      return ofy().load().list();
+    } else {
+      return jpaTm()
+          .transact(
+              () -> {
+                ImmutableList<? extends Class<?>> entityClasses =
+                    jpaTm().getEntityManager().getMetamodel().getEntities().stream()
+                        .map(javax.persistence.metamodel.Type::getJavaType)
+                        .collect(toImmutableList());
+                ImmutableList.Builder<Object> result = new ImmutableList.Builder<>();
+                for (Class<?> entityClass : entityClasses) {
+                  for (Object object : jpaTm().loadAllOf(entityClass)) {
+                    // Initialize and detach the objects so they can be used for comparison later
+                    initializeObject(object);
+                    jpaTm().getEntityManager().detach(object);
+                    result.add(object);
+                  }
+                }
+                return result.build();
+              });
+    }
+  }
+
+  private static void initializeObject(Object object) {
+    // We must initialize all possible collection fields in this object if we wish to
+    // use them outside of the transaction. Use reflection to initialize all fields
+    // for all objects.
+    for (Field field : object.getClass().getDeclaredFields()) {
+      field.setAccessible(true);
+      try {
+        Hibernate.initialize(field.get(object));
+      } catch (IllegalAccessException e) {
+        // shouldn't happen since we set the field to be accessible
+      }
+    }
   }
 
   /**
