8b1c9e16bf819aa8f9b9352cb2ab7a2e7adcf3a5
Lai Jiang <jianglai@msn.com>
2021-04-21 09:57:20 -0400

    Update PublishInvoicesAction

--- core/src/main/java/google/registry/reporting/billing/PublishInvoicesAction.java
+++ core/src/main/java/google/registry/reporting/billing/PublishInvoicesAction.java
@@ -59,6 +59,7 @@ public class PublishInvoicesAction implements Runnable {
   private static final String JOB_FAILED = "JOB_STATE_FAILED";
 
   private final String projectId;
+  private final String jobRegion;
   private final String jobId;
   private final BillingEmailUtils emailUtils;
   private final Dataflow dataflow;
@@ -69,11 +70,13 @@ public class PublishInvoicesAction implements Runnable {
   PublishInvoicesAction(
       @Config("projectId") String projectId,
       @Parameter(ReportingModule.PARAM_JOB_ID) String jobId,
+      @Config("defaultJobRegion") String jobRegion,
       BillingEmailUtils emailUtils,
       Dataflow dataflow,
       Response response,
       YearMonth yearMonth) {
     this.projectId = projectId;
+    this.jobRegion = jobRegion;
     this.jobId = jobId;
     this.emailUtils = emailUtils;
     this.dataflow = dataflow;
@@ -87,7 +90,7 @@ public class PublishInvoicesAction implements Runnable {
   public void run() {
     try {
       logger.atInfo().log("Starting publish job.");
-      Job job = dataflow.projects().jobs().get(projectId, jobId).execute();
+      Job job = dataflow.projects().locations().jobs().get(projectId, jobRegion, jobId).execute();
       String state = job.getCurrentState();
       switch (state) {
         case JOB_DONE:
