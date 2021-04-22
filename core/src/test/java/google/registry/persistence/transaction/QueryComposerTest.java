e441d833e865bc7fdc585494a7d1fa52c6857aff
Michael Muller <mmuller@google.com>
2021-04-22 08:48:32 -0400

    Add test for list queries

--- core/src/test/java/google/registry/persistence/transaction/QueryComposerTest.java
+++ core/src/test/java/google/registry/persistence/transaction/QueryComposerTest.java
@@ -207,6 +207,18 @@ public class QueryComposerTest {
   }
 
   @TestOfyAndSql
+  public void testListQueries() {
+    assertThat(
+            transactIfJpaTm(
+                () ->
+                    tm()
+                        .createQueryComposer(TestEntity.class)
+                        .where("name", Comparator.GT, "alpha")
+                        .list()))
+        .isEqualTo(ImmutableList.of(bravo, charlie));
+  }
+
+  @TestOfyAndSql
   public void testNonPrimaryKey() {
     assertThat(
             transactIfJpaTm(
