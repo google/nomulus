1f3481880d441b390849835375fa86cb6a08b0c1
rachelguan <rachelguan@google.com>
2021-04-30 19:25:16 +0000

    change premium list command to be based off of mutating command

--- core/src/test/java/google/registry/tools/UpdatePremiumListCommandTest.java
+++ core/src/test/java/google/registry/tools/UpdatePremiumListCommandTest.java
@@ -14,60 +14,148 @@
 
 package google.registry.tools;
 
-import static google.registry.request.JsonResponse.JSON_SAFETY_PREFIX;
-import static google.registry.testing.TestDataHelper.loadFile;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
+import static com.google.common.truth.Truth.assertThat;
+import static google.registry.model.registry.Registry.TldState.GENERAL_AVAILABILITY;
+import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
+import static google.registry.testing.DatabaseHelper.newRegistry;
+import static google.registry.testing.DatabaseHelper.persistPremiumList;
+import static google.registry.testing.DatabaseHelper.persistResource;
+import static google.registry.util.DateTimeUtils.START_OF_TIME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.net.MediaType;
-import google.registry.tools.server.UpdatePremiumListAction;
+import com.google.common.base.Ascii;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.common.io.Files;
+import google.registry.dns.writer.VoidDnsWriter;
+import google.registry.model.pricing.StaticPremiumListPricingEngine;
+import google.registry.model.registry.Registry;
+import google.registry.model.registry.Registry.TldType;
+import google.registry.model.registry.label.PremiumList;
+import google.registry.model.registry.label.PremiumListDualDao;
+import java.io.File;
+import java.math.BigDecimal;
+import java.nio.file.Paths;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 /** Unit tests for {@link UpdatePremiumListCommand}. */
 class UpdatePremiumListCommandTest<C extends UpdatePremiumListCommand>
     extends CreateOrUpdatePremiumListCommandTestCase<C> {
-
-  @Mock AppEngineConnection connection;
-
-  private String premiumTermsPath;
-  String premiumTermsCsv;
-  private String servletPath;
+  Registry registry;
 
   @BeforeEach
-  void beforeEach() throws Exception {
-    command.setConnection(connection);
-    servletPath = "/_dr/admin/updatePremiumList";
-    premiumTermsPath =
-        writeToNamedTmpFile(
-            "example_premium_terms.csv",
-            loadFile(UpdatePremiumListCommandTest.class, "example_premium_terms.csv"));
-    when(connection.sendPostRequest(
-            eq(UpdatePremiumListAction.PATH), anyMap(), any(MediaType.class), any(byte[].class)))
-        .thenReturn(JSON_SAFETY_PREFIX + "{\"status\":\"success\",\"lines\":[]}");
+  void beforeEach() {
+    registry =
+        newRegistry(
+            TLD_TEST,
+            Ascii.toUpperCase(TLD_TEST),
+            ImmutableSortedMap.of(START_OF_TIME, GENERAL_AVAILABILITY),
+            TldType.TEST);
+    persistPremiumList(TLD_TEST, "foo,USD 100");
+    persistResource(registry);
+  }
+
+  // TODO: this wont work til the problem below gets fixed;
+  @Test
+  void testSuccess_updateList() throws Exception {
+    // ensure that no premium list is created before running the command
+    assertThat(PremiumListDualDao.exists(TLD_TEST)).isTrue();
+    assertThat(PremiumListDualDao.getLatestRevision(TLD_TEST).isPresent()).isTrue();
+    File tmpFile = tmpDir.resolve(TLD_TEST+".txt").toFile();
+    String premiumTermsCsv = "foo,USD 9000";
+    Files.asCharSink(tmpFile, UTF_8).write(premiumTermsCsv);
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    command.inputFile = Paths.get(tmpFile.getPath());
+    runCommandForced("--name=" + TLD_TEST, "--input=" + command.inputFile);
+
+    PremiumList persistedList = PremiumListDualDao.getLatestRevision(TLD_TEST).get();
+    assertThat(persistedList.getLabelsToPrices()).
+        containsEntry("foo", new BigDecimal("9000.00"));
   }
 
   @Test
-  void testRun() throws Exception {
-    runCommandForced("-i=" + premiumTermsPath, "-n=foo");
-    verifySentParams(
-        connection,
-        servletPath,
-        ImmutableMap.of("name", "foo", "inputData", generateInputData(premiumTermsPath)));
+  // TODO: figure out why this is not working and fix it ofc
+  /*
+  during debugging, there's a problem with labelsToPrices =
+  {PersistentMap@10685} Unable to evaluate the expression Method
+  threw 'org.hibernate.LazyInitizationException' exception;
+  this.value = {PremiumList@10563} Method
+  threw 'org.hibernate.LazyInitializationException' exception.
+  Cannot evaluate google.registry.model.registry.label.PremiumList.toString()'
+
+  * */
+  void testSuccess_stageEntityChange() throws Exception {
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    command.inputFile = Paths.get(premiumTermsPath);
+    command.name = TLD_TEST;
+    command.init();
+    assertThat(command.prompt()).contains("Update PremiumList@");
+  }
+
+  /*
+  TODO: same as above
+  * */
+  @Test
+  void testSuccess_stageNoEntityChange() throws Exception {
+    File tmpFile = tmpDir.resolve("prime.txt").toFile();
+    String premiumTermsCsv = "foo,USD 100";
+    Files.asCharSink(tmpFile, UTF_8).write(premiumTermsCsv);
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    command.inputFile = Paths.get(tmpFile.getPath());
+    command.name = TLD_TEST;
+    command.init();
+    assertThat(command.prompt()).contains("No entity changes to apply.");
   }
 
   @Test
-  void testRun_noProvidedName_usesBasenameOfInputFile() throws Exception {
-    runCommandForced("-i=" + premiumTermsPath);
-    assertInStdout("Successfully");
-    verifySentParams(
-        connection,
-        servletPath,
-        ImmutableMap.of(
-            "name", "example_premium_terms", "inputData", generateInputData(premiumTermsPath)));
+  void testFailure_noPreviousVersion() {
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    String randomStr = "random";
+    command.name = randomStr;
+    Registry tmpRegistry =
+        new Registry.Builder()
+            .setTldStr(randomStr)
+            .setPremiumPricingEngine(StaticPremiumListPricingEngine.NAME)
+            .setDnsWriters(ImmutableSet.of(VoidDnsWriter.NAME))
+            .setPremiumList(null)
+            .build();
+    tm().transact(() -> tm().put(tmpRegistry));
+    IllegalArgumentException thrown =
+        assertThrows(IllegalArgumentException.class, command::init);
+    assertThat(thrown)
+        .hasMessageThat()
+        .isEqualTo(
+            String.format(
+                "Could not update premium list %s because it doesn't exist.", randomStr));
+  }
+
+  @Test
+  void testFailure_noInputFile() {
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    assertThrows(NullPointerException.class, command::init);
+  }
+
+  @Test
+  void testFailure_tldDoesNotExist_fileName() {
+    String randomStr = "random";
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    command.name = randomStr;
+    IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, command::init);
+    assertThat(thrown)
+        .hasMessageThat().isEqualTo( String.format(
+        "Could not update premium list %s because it doesn't exist.", randomStr));
+  }
+
+  @Test
+  void testFailure_tldDoesNotExist_name() {
+    String randomStr = "random";
+    UpdatePremiumListCommand command = new UpdatePremiumListCommand();
+    command.inputFile = Paths.get(tmpDir.resolve(randomStr+".txt").toFile().getPath());
+    IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, command::init);
+    assertThat(thrown)
+        .hasMessageThat().isEqualTo( String.format(
+        "Could not update premium list %s because it doesn't exist.", randomStr));
   }
 }
