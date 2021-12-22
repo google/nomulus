78cc57ed480cac146ef92cde75c0e609c0188309
Lai Jiang <jianglai@google.com>
2021-12-21 23:33:12 -0500

    Add a command to compare two escrow  deposits
    We already have ValidateEscrowDepositCommand to check for internal
    reference consistency of two deposits, i. e. making sure that all
    contacts and hosts referenced by domains exist in the same deposit.
    Therefore to compare whether two deposits are equal we only need to make
    sure that they contain the same domains and registrars, assuming they
    both pass the validation. We don't compare their contents directly
    because the MapReduce deposit contains all contacts and domains whereas
    the Beam deposit only contains referenced ones, making a direct
    comparison impossible.

--- core/src/main/java/google/registry/tools/RegistryToolComponent.java
+++ core/src/main/java/google/registry/tools/RegistryToolComponent.java
@@ -42,6 +42,7 @@ import google.registry.request.Modules.UrlFetchTransportModule;
 import google.registry.request.Modules.UserServiceModule;
 import google.registry.tools.AuthModule.LocalCredentialModule;
 import google.registry.tools.javascrap.BackfillRegistryLocksCommand;
+import google.registry.tools.javascrap.CompareEscrowDepositsCommand;
 import google.registry.tools.javascrap.DeleteContactByRoidCommand;
 import google.registry.tools.javascrap.HardDeleteHostCommand;
 import google.registry.util.UtilsModule;
@@ -92,6 +93,8 @@ interface RegistryToolComponent {
   void inject(CheckDomainClaimsCommand command);
 
   void inject(CheckDomainCommand command);
+
+  void inject(CompareEscrowDepositsCommand command);
 
   void inject(CountDomainsCommand command);
 
