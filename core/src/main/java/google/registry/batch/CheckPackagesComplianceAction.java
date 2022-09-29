4f4a4b120b1b58a00bd781513b9ea8d01dae7ba6
Sarah Botwinick <sarahbot@google.com>
2022-09-29 12:02:19 -0400

    Add create limit monitoring

--- core/src/main/java/google/registry/batch/CheckPackagesComplianceAction.java
+++ core/src/main/java/google/registry/batch/CheckPackagesComplianceAction.java
@@ -18,10 +18,12 @@ import static google.registry.persistence.transaction.TransactionManagerFactory.
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.FluentLogger;
+import google.registry.model.domain.DomainHistory;
 import google.registry.model.domain.token.PackagePromotion;
 import google.registry.request.Action;
 import google.registry.request.Action.Service;
 import google.registry.request.auth.Auth;
+import java.util.List;
 
 /**
  * An action that checks all {@link PackagePromotion} objects for compliance with their max create
@@ -42,32 +44,30 @@ public class CheckPackagesComplianceAction implements Runnable {
         tm().transact(() -> tm().loadAllOf(PackagePromotion.class));
     ImmutableList.Builder<PackagePromotion> packagesOverCreateLimit = new ImmutableList.Builder<>();
     for (PackagePromotion packagePromo : packages) {
-      // load all domainHistory events with packagetoken equal to package's token and is a create.
-      // And occurred since 1 year before nextBillingDate
-      int createCount =
-          (Integer)
-              jpaTm()
-                  .transact(
-                      () ->
-                          jpaTm()
-                              .getEntityManager()
-                              .createQuery(
-                                  "SELECT COUNT(*) FROM PackagePromotion WHERE currentPackageToken"
-                                      + " = :token AND creationTime >= :creationTime AND"
-                                      + " historyType = DOMAIN_CREATE")
-                              .setParameter("token", packagePromo.getToken().getSqlKey())
-                              .setParameter(
-                                  "creationTime", packagePromo.getNextBillingDate().minusYears(1))
-                              .getSingleResult());
-      if (createCount > packagePromo.getMaxCreates()) {
+      List<DomainHistory> creates =
+          jpaTm()
+              .transact(
+                  () ->
+                      jpaTm()
+                          .query(
+                              "FROM DomainHistory WHERE current_package_token = :token AND"
+                                  + " modificationTime >= :creation AND type = 'DOMAIN_CREATE'",
+                              DomainHistory.class)
+                          .setParameter("token", packagePromo.getToken().getSqlKey().toString())
+                          .setParameter("creation", packagePromo.getNextBillingDate().minusYears(1))
+                          .getResultList());
+
+      if (creates.size() > packagePromo.getMaxCreates()) {
         packagesOverCreateLimit.add(packagePromo);
       }
     }
     if (packagesOverCreateLimit.build().isEmpty()) {
-      logger.atInfo().log("Found 0 packages over their create limit.");
+      logger.atInfo().log("Found no packages over their create limit.");
     } else {
       logger.atInfo().log(
           "Found %d packages over their create limit.", packagesOverCreateLimit.build().size());
+      // TODO(sarahbot@) Send email to registrar and registry informing of creation overage once
+      // email template is finalized.
     }
   }
 }
