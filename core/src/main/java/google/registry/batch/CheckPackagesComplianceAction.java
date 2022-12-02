74581dbb515fa2630837e683338c5b2dd9df00cc
Sarah Botwinick <sarahbot@google.com>
2022-12-02 15:16:59 -0500

    Add monitoring for package active domain limits

--- core/src/main/java/google/registry/batch/CheckPackagesComplianceAction.java
+++ core/src/main/java/google/registry/batch/CheckPackagesComplianceAction.java
@@ -15,6 +15,7 @@ package google.registry.batch;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
+import static google.registry.util.DateTimeUtils.END_OF_TIME;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.FluentLogger;
@@ -28,6 +29,7 @@ import google.registry.request.Action;
 import google.registry.request.Action.Service;
 import google.registry.request.auth.Auth;
 import google.registry.ui.server.SendEmailUtils;
+import google.registry.util.Clock;
 import java.util.Optional;
 import javax.inject.Inject;
 
@@ -44,19 +46,34 @@ public class CheckPackagesComplianceAction implements Runnable {
   public static final String PATH = "/_dr/task/checkPackagesCompliance";
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   private final SendEmailUtils sendEmailUtils;
-  private final String packageCreateLimitEmailSubjectText;
-  private final String packageCreateLimitEmailBodyText;
+  private final Clock clock;
+  private final String packageCreateLimitEmailSubject;
+  private final String packageDomainLimitWarningEmailSubject;
+  private final String packageDomainLimitUpgradeEmailSubject;
+  private final String packageCreateLimitEmailBody;
+  private final String packageDomainLimitWarningEmailBody;
+  private final String packageDomainLimitUpgradeEmailBody;
   private final String registrySupportEmail;
 
   @Inject
   public CheckPackagesComplianceAction(
       SendEmailUtils sendEmailUtils,
-      @Config("packageCreateLimitEmailSubjectText") String packageCreateLimitEmailSubjectText,
-      @Config("packageCreateLimitEmailBodyText") String packageCreateLimitEmailBodyText,
+      Clock clock,
+      @Config("packageCreateLimitEmailSubject") String packageCreateLimitEmailSubject,
+      @Config("packageDomainLimitWarningEmailSubject") String packageDomainLimitWarningEmailSubject,
+      @Config("packageDomainLimitUpgradeEmailSubject") String packageDomainLimitUpgradeEmailSubject,
+      @Config("packageCreateLimitEmailBody") String packageCreateLimitEmailBody,
+      @Config("packageDomainLimitWarningEmailBody") String packageDomainLimitWarningEmailBody,
+      @Config("packageDomainLimitUpgradeEmailBody") String packageDomainLimitUpgradeEmailBody,
       @Config("registrySupportEmail") String registrySupportEmail) {
     this.sendEmailUtils = sendEmailUtils;
-    this.packageCreateLimitEmailSubjectText = packageCreateLimitEmailSubjectText;
-    this.packageCreateLimitEmailBodyText = packageCreateLimitEmailBodyText;
+    this.clock = clock;
+    this.packageCreateLimitEmailSubject = packageCreateLimitEmailSubject;
+    this.packageDomainLimitWarningEmailSubject = packageDomainLimitWarningEmailSubject;
+    this.packageDomainLimitUpgradeEmailSubject = packageDomainLimitUpgradeEmailSubject;
+    this.packageCreateLimitEmailBody = packageCreateLimitEmailBody;
+    this.packageDomainLimitWarningEmailBody = packageDomainLimitWarningEmailBody;
+    this.packageDomainLimitUpgradeEmailBody = packageDomainLimitUpgradeEmailBody;
     this.registrySupportEmail = registrySupportEmail;
   }
 
@@ -66,6 +83,8 @@ public class CheckPackagesComplianceAction implements Runnable {
             () -> {
               ImmutableList<PackagePromotion> packages = tm().loadAllOf(PackagePromotion.class);
               ImmutableList.Builder<PackagePromotion> packagesOverCreateLimit =
+                  new ImmutableList.Builder<>();
+              ImmutableList.Builder<PackagePromotion> packagesOverActiveDomainsLimit =
                   new ImmutableList.Builder<>();
               for (PackagePromotion packagePromo : packages) {
                 Long creates =
@@ -86,6 +105,24 @@ public class CheckPackagesComplianceAction implements Runnable {
                       packagePromo.getToken().getKey(), overage);
                   packagesOverCreateLimit.add(packagePromo);
                 }
+
+                Long activeDomains =
+                    (Long)
+                        tm().query(
+                                "SELECT COUNT(*) FROM Domain WHERE current_package_token = :token"
+                                    + " AND deletion_time = :time")
+                            .setParameter("token", packagePromo.getToken().getKey().toString())
+                            .setParameter("time", END_OF_TIME.toDate())
+                            .getSingleResult();
+
+                if (activeDomains > packagePromo.getMaxDomains()) {
+                  int overage = Ints.saturatedCast(activeDomains) - packagePromo.getMaxDomains();
+                  logger.atInfo().log(
+                      "Package with package token %s has exceed their max active domains limit by"
+                          + " %d name(s).",
+                      packagePromo.getToken().getKey(), overage);
+                  packagesOverActiveDomainsLimit.add(packagePromo);
+                }
               }
               if (packagesOverCreateLimit.build().isEmpty()) {
                 logger.atInfo().log("Found no packages over their create limit.");
@@ -101,12 +138,73 @@ public class CheckPackagesComplianceAction implements Runnable {
                   if (registrar.isPresent()) {
                     String body =
                         String.format(
-                            packageCreateLimitEmailBodyText,
+                            packageCreateLimitEmailBody,
                             registrar.get().getRegistrarName(),
                             packageToken.getToken(),
                             registrySupportEmail);
                     sendNotification(
-                        packageToken, packageCreateLimitEmailSubjectText, body, registrar.get());
+                        packageToken, packageCreateLimitEmailSubject, body, registrar.get());
+                  } else {
+                    logger.atSevere().log(
+                        String.format(
+                            "Could not find registrar for package token %s", packageToken));
+                  }
+                }
+              }
+
+              if (packagesOverActiveDomainsLimit.build().isEmpty()) {
+                logger.atInfo().log("Found no packages over their active domains limit.");
+              } else {
+                logger.atInfo().log(
+                    "Found %d packages over their active domains limit.",
+                    packagesOverActiveDomainsLimit.build().size());
+                for (PackagePromotion packagePromotion : packagesOverActiveDomainsLimit.build()) {
+                  // Determine if email should be 30 day warning or upgrade notification
+                  String emailTemplate;
+                  String emailSubject;
+                  if (packagePromotion.getLastNotificationSent().isPresent()) {
+                    if (packagePromotion
+                        .getLastNotificationSent()
+                        .get()
+                        .isAfter(clock.nowUtc().minusDays(30))) {
+                      // Don't send an email if notification was already sent within the last 30
+                      // days
+                      continue;
+                    } else if (packagePromotion
+                        .getLastNotificationSent()
+                        .get()
+                        .isBefore(clock.nowUtc().minusDays(40))) {
+                      // Send a warning if last warning email was more than 40 days ago
+                      emailTemplate = packageDomainLimitWarningEmailBody;
+                      emailSubject = packageDomainLimitWarningEmailSubject;
+                    } else {
+                      // Send an upgrade email if last email was between 30 and 40 days ago
+                      emailTemplate = packageDomainLimitUpgradeEmailBody;
+                      emailSubject = packageDomainLimitUpgradeEmailSubject;
+                    }
+                  } else {
+                    // if no previous email has been sent, send a warning email
+                    emailTemplate = packageDomainLimitWarningEmailBody;
+                    emailSubject = packageDomainLimitWarningEmailSubject;
+                  }
+
+                  AllocationToken packageToken = tm().loadByKey(packagePromotion.getToken());
+                  Optional<Registrar> registrar =
+                      Registrar.loadByRegistrarIdCached(
+                          packageToken.getAllowedRegistrarIds().iterator().next());
+                  if (registrar.isPresent()) {
+                    String body =
+                        String.format(
+                            emailTemplate,
+                            registrar.get().getRegistrarName(),
+                            packageToken.getToken(),
+                            registrySupportEmail);
+                    sendNotification(packageToken, emailSubject, body, registrar.get());
+                    tm().put(
+                            packagePromotion
+                                .asBuilder()
+                                .setLastNotificationSent(clock.nowUtc())
+                                .build());
                   } else {
                     logger.atSevere().log(
                         String.format(
