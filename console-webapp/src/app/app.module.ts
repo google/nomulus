90bc60aca1b535ebd8be39924dc3f73bdda8ad02
ptkach <ptkach@google.com>
2023-03-14 18:00:02 +0000

    Header initialized

--- console-webapp/src/app/app.module.ts
+++ console-webapp/src/app/app.module.ts
@@ -18,16 +18,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
+import { MaterialModule } from './material.module';
 
 import { HomeComponent } from './home/home.component';
 import { TldsComponent } from './tlds/tlds.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TldsComponent,
+    HeaderComponent,
   ],
   imports: [
     MaterialModule,
