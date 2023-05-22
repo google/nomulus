6adaad8aab5e1d9ef5cee8395b4bf022ed8a01dc
Sarah Botwinick <sarahbot@google.com>
2023-05-22 16:40:02 -0400

    Add batching to the query

--- core/src/main/java/google/registry/tools/server/RefreshDnsForAllDomainsAction.java
+++ core/src/main/java/google/registry/tools/server/RefreshDnsForAllDomainsAction.java
@@ -15,11 +15,13 @@
 package google.registry.tools.server;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static google.registry.dns.DnsUtils.requestDomainDnsRefresh;
 import static google.registry.model.tld.Registries.assertTldsExist;
 import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
 import static google.registry.request.RequestParameters.PARAM_TLDS;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.flogger.FluentLogger;
 import google.registry.request.Action;
@@ -29,7 +31,6 @@ import google.registry.request.auth.Auth;
 import google.registry.util.Clock;
 import java.util.Random;
 import javax.inject.Inject;
-import javax.persistence.TypedQuery;
 import org.apache.http.HttpStatus;
 import org.joda.time.Duration;
 
@@ -57,6 +58,8 @@ public class RefreshDnsForAllDomainsAction implements Runnable {
 
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
+  static final int BATCH_SIZE = 10;
+
   @Inject Response response;
 
   @Inject
@@ -66,11 +69,6 @@ public class RefreshDnsForAllDomainsAction implements Runnable {
   @Inject
   @Parameter("smearMinutes")
   int smearMinutes;
-
-  /** Include deleted domain names. */
-  @Inject
-  @Parameter("includeDeleted")
-  boolean includeDeleted;
 
   @Inject Clock clock;
   @Inject Random random;
@@ -82,31 +80,39 @@ public class RefreshDnsForAllDomainsAction implements Runnable {
   public void run() {
     assertTldsExist(tlds);
     checkArgument(smearMinutes > 0, "Must specify a positive number of smear minutes");
-    StringBuilder query = new StringBuilder("SELECT domainName FROM Domain WHERE tld IN (:tlds)");
-    if (!includeDeleted) {
-      query.append(" AND deletionTime > :now");
+    String lastInPreviousBatch = "";
+    while (true) {
+      String previousLastName = lastInPreviousBatch;
+      ImmutableList<String> domainBatch =
+          ImmutableList.copyOf(
+              tm().transact(
+                      () ->
+                          tm().query(
+                                  "SELECT domainName FROM Domain WHERE tld IN (:tlds) AND"
+                                      + " deletionTime > :now  AND domainName >"
+                                      + " :lastInPreviousBatch ORDER BY domainName ASC",
+                                  String.class)
+                              .setParameter("tlds", tlds)
+                              .setParameter("now", clock.nowUtc())
+                              .setParameter("lastInPreviousBatch", previousLastName)
+                              .setMaxResults(BATCH_SIZE)
+                              .getResultStream()
+                              .collect(toImmutableList())));
+
+      if (domainBatch.isEmpty()) break;
+      domainBatch.forEach(
+          domainName -> {
+            try {
+              // Smear the task execution time over the next N minutes.
+              requestDomainDnsRefresh(
+                  domainName, Duration.standardMinutes(random.nextInt(smearMinutes)));
+            } catch (Throwable t) {
+              logger.atSevere().withCause(t).log(
+                  "Error while enqueuing DNS refresh for domain '%s'.", domainName);
+              response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+            }
+          });
+      lastInPreviousBatch = domainBatch.get(domainBatch.size() - 1);
     }
-    tm().transact(
-            () -> {
-              TypedQuery<String> typedQuery =
-                  tm().query(query.toString(), String.class).setParameter("tlds", tlds);
-              if (!includeDeleted) {
-                typedQuery.setParameter("now", clock.nowUtc());
-              }
-              typedQuery
-                  .getResultStream()
-                  .forEach(
-                      domainName -> {
-                        try {
-                          // Smear the task execution time over the next N minutes.
-                          requestDomainDnsRefresh(
-                              domainName, Duration.standardMinutes(random.nextInt(smearMinutes)));
-                        } catch (Throwable t) {
-                          logger.atSevere().withCause(t).log(
-                              "Error while enqueuing DNS refresh for domain '%s'.", domainName);
-                          response.setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
-                        }
-                      });
-            });
   }
 }
