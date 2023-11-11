161b7d1c9d32a01007c34fe7838e02e0df9b9f76
Weimin Yu <weiminyu@google.com>
2023-11-11 01:13:02 +0000

    Apply BSA diffs to the database
    Add a step in BsaDownloadAction to apply the diffs from the most recent
    download. This includes:
    
    - Add new labels and remove deleted labels to the `BsaLabel` table.
    - Add new entries to the `BsaDomainInUse` table if applicable. These are
      registered/reserved domains that match the new labels.
    - Generate list of domains that are not blocked and save them in a GCS
      file. These include all domains from the previous step as well as
      domains derived from invalid labels.
    
    More tests are needed but this PR successfully processed a real
    download.

--- core/src/main/java/google/registry/bsa/BlockListFetcher.java
+++ core/src/main/java/google/registry/bsa/BlockListFetcher.java
@@ -75,7 +75,7 @@ public class BlockListFetcher {
         try (InputStream errorStream = connection.getErrorStream()) {
           errorDetails = new String(ByteStreams.toByteArray(errorStream), UTF_8);
         } catch (Exception e) {
-          // ignore
+          errorDetails = "Failed to retrieve error message: " + e.getMessage();
         }
         throw new BsaException(
             String.format(
