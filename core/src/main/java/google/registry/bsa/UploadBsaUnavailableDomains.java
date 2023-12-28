8327bf7e71b69ac68bb1055258235d02f5490322
Ben McIlwain <mcilwain@google.com>
2023-12-28 16:32:45 -0500

    Add a daily batch action to upload unavailable domains to BSA

---
+++ core/src/main/java/google/registry/bsa/UploadBsaUnavailableDomains.java
@@ -0,0 +1,229 @@
+// Copyright 2023 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package google.registry.bsa;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static google.registry.model.tld.Tld.isEnrolledWithBsa;
+import static google.registry.model.tld.Tlds.getTldEntitiesOfType;
+import static google.registry.model.tld.label.ReservedList.loadReservedLists;
+import static google.registry.persistence.transaction.TransactionManagerFactory.replicaTm;
+import static google.registry.request.Action.Method.POST;
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
+import com.google.api.client.http.HttpMethods;
+import com.google.cloud.storage.BlobId;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Ordering;
+import com.google.common.flogger.FluentLogger;
+import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSource;
+import google.registry.config.RegistryConfig.Config;
+import google.registry.gcs.GcsUtils;
+import google.registry.model.tld.Tld;
+import google.registry.model.tld.Tld.TldType;
+import google.registry.model.tld.label.ReservedList;
+import google.registry.request.Action;
+import google.registry.request.Action.Service;
+import google.registry.request.UrlConnectionService;
+import google.registry.request.auth.Auth;
+import google.registry.util.Clock;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import javax.inject.Inject;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.joda.time.DateTime;
+
+/**
+ * Daily action that uploads unavailable domain names on applicable TLDs to BSA.
+ *
+ * <p>The upload is a single zipped text file containing combined details for all BSA-enrolled TLDs.
+ * The text is a newline-delimited list of punycoded fully qualified domain names, and contains all
+ * domains on each TLD that are registered and/or reserved.
+ *
+ * <p>The file is also uploaded to GCS to preserve it as a record for ourselves.
+ */
+@Action(
+    service = Service.BSA,
+    path = "/_dr/task/uploadBsaUnavailableNames",
+    method = POST,
+    auth = Auth.AUTH_API_ADMIN)
+public class UploadBsaUnavailableDomains implements Runnable {
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  Clock clock;
+
+  UrlConnectionService urlConnectionService;
+
+  GcsUtils gcsUtils;
+
+  String gcsBucket;
+
+  @Inject
+  public UploadBsaUnavailableDomains(
+      Clock clock,
+      UrlConnectionService urlConnectionService,
+      GcsUtils gcsUtils,
+      @Config("bsaUnavailableDomainsGcsBucket") String gcsBucket) {
+    this.clock = clock;
+    this.urlConnectionService = urlConnectionService;
+    this.gcsUtils = gcsUtils;
+    this.gcsBucket = gcsBucket;
+  }
+
+  @Override
+  public void run() {
+    DateTime runTime = clock.nowUtc();
+    String unavailableDomains =
+        Joiner.on("\n").join(replicaTm().transact(() -> getUnavailableDomains(runTime)));
+    uploadToGcs(unavailableDomains, runTime);
+    uploadToBsa(unavailableDomains, runTime);
+  }
+
+  /** Uploads the unavailable domains list to GCS in the unavailable domains bucket. */
+  void uploadToGcs(String unavailableDomains, DateTime runTime) {
+    BlobId blobId = BlobId.of(gcsBucket, runTime.toString() + ".txt");
+    try (OutputStream gcsOutput = gcsUtils.openOutputStream(blobId);
+        Writer osWriter = new OutputStreamWriter(gcsOutput, US_ASCII)) {
+      osWriter.write(unavailableDomains);
+    } catch (Throwable e) {
+      logger.atSevere().withCause(e).log(
+          "Error writing BSA unavailable domains to GCS; skipping to BSA upload ...");
+    }
+  }
+
+  void uploadToBsa(String unavailableDomains, DateTime runTime) {
+    // POST to https://{Environment_Hostname}/bsa/api/zonefile
+    // Body: form-data
+    // 1 Key: zone
+    // a Value: the checksum of the file that is provided, e.g.
+    //
+    // {"checkSum":
+    // "b5819d248a055912213107f3090a3c1c8a9c2cfdc053b56661cf7a1735cf4c8192a0f855ab5a8
+    // 10adc897ffe639d1243e777ac81a796b81c21fe208982d78a50"}
+    //
+    // b Content-type: application/json
+    // 2 Key: file
+    // a Value: the compressed unavailable names file
+
+    // CURL example:
+    // curl --location 'https://{Environment_Hostname}/bsa/api/zonefile' --header
+    // 'Authorization: Bearer TOKEN_GOES_HERE' --form 'zone="{\"checkSum\":
+    // "CHECKSUM_GOES_HERE"}";type=application/json' --form
+    // 'file=@"FILE_REF_GOES_HERE"'
+
+    // Write encrypted file to bytes.
+    // ByteSource(construct).hash(Hashing.sha512())
+
+    try {
+      // Need to hash the compressed file.
+      String hash =
+          ByteSource.wrap(unavailableDomains.getBytes(US_ASCII)).hash(Hashing.sha512()).toString();
+      String url = String.format("%s/%s", "TODO base URL", "bsa/api/zonefile");
+      String filename = "TODO";
+
+
+      OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+      RequestBody body =
+          new MultipartBody.Builder()
+              .setType(MultipartBody.FORM)
+              .addFormDataPart(
+                  "zone",
+                  null,
+                  RequestBody.create(String.format("{\"checkSum\": \"%s\"}", hash).getBytes(),
+                      MediaType.parse("application/json")))
+              .addFormDataPart(
+                  "file",
+                  "example.csv.gz",
+                  RequestBody.create(
+                      new File("D://example.csv.gz"), MediaType.parse("application/octet-stream")))
+              .build();
+
+      String authBearerToken = "TODO";
+      Request request =
+          new Request.Builder()
+              .url(url)
+              .method("POST", body)
+              .addHeader("Authorization", "Bearer " + authBearerToken)
+              .build();
+
+      logger.atInfo().log("Uploading unavailable domains list %s to %s with hash %s", filename, url, hash);
+      try (Response response = client.newCall(request).execute()) {
+        logger.atInfo().log("Received response with code %s from server: %s", response.code(), response.body().string());
+      }
+
+      HttpURLConnection connection = urlConnectionService.createConnection(new URL(url));
+      connection.setRequestMethod(HttpMethods.POST);
+
+      // Files.hash(new File(fileName), Hashing.sha512()).toString();
+      connection.setRequestProperty(AUTHORIZATION, "token here");
+      // UrlConnectionUtils.setPayloadMultipart(connection, "file", "filename", "application/json");
+    } catch (IOException | GeneralSecurityException e) {
+      // ...
+    }
+  }
+
+  private ImmutableSortedSet<String> getUnavailableDomains(DateTime runTime) {
+    ImmutableSet<Tld> bsaEnabledTlds =
+        getTldEntitiesOfType(TldType.REAL).stream()
+            .filter(tld -> isEnrolledWithBsa(tld, runTime))
+            .collect(toImmutableSet());
+
+    ImmutableSortedSet.Builder<String> unavailableDomains =
+        new ImmutableSortedSet.Builder<>(Ordering.natural());
+    for (Tld tld : bsaEnabledTlds) {
+      for (ReservedList reservedList : loadReservedLists(tld.getReservedListNames())) {
+        if (reservedList.getShouldPublish()) {
+          unavailableDomains.addAll(
+              reservedList.getReservedListEntries().keySet().stream()
+                  .map(label -> toDomain(label, tld))
+                  .collect(toImmutableSet()));
+        }
+      }
+    }
+
+    unavailableDomains.addAll(
+        replicaTm()
+            .query(
+                "SELECT domainName FROM Domain "
+                    + "WHERE tld IN :tlds "
+                    + "AND deletionTime > :now ",
+                String.class)
+            .setParameter(
+                "tlds", bsaEnabledTlds.stream().map(Tld::getTldStr).collect(toImmutableSet()))
+            .setParameter("now", runTime)
+            .getResultList());
+    return unavailableDomains.build();
+  }
+
+  private static String toDomain(String domainLabel, Tld tld) {
+    return String.format("%s.%s", domainLabel, tld.getTldStr());
+  }
+}
