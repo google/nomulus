c5d9b211e059b38d9ac554048ebcfcc35930ea63
Sarah Botwinick <sarahbot@google.com>
2024-01-24 17:20:08 -0500

    Add build_environment flag

--- release/db-object-updater/sync_db_objects.sh
+++ release/db-object-updater/sync_db_objects.sh
@@ -35,9 +35,7 @@ nomulus_command="${4}"
 
 echo ${config_file_directory}
 
-#TODO(sarahbot@): Add the --build_environment flag once go/r3pr/2292 and go/r3pr/2277 are deployed
-#for FILE in ${config_file_directory}/${nomulus_env}/*; do
-for FILE in ${config_file_directory}/sandbox/*; do
+for FILE in ${config_file_directory}/${nomulus_env}/*; do
   echo $FILE
   java -jar /nomulus.jar -e "${nomulus_env}" \
   --credential "${tools_credential}" \
