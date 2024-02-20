340a7661860912407362fc13e43bc0d92beec86e
Gus Brodman <gbrodman@google.com>
2024-02-20 15:01:43 -0500

    Add an example UserUpdateHistory class and SQL
    This won't pass the build because it doesn't follow the necessary logic
    of adding the SQL code to the golden file first, but it serves as an
    example of what the code for console update history objects will look
    like.
    
    Notably:
    - We split User, adding AbstractUser, to store an AbstractUser in
      UserUpdateHistory (analogous to DomainBase/Domain et al.)
    - We have an abstract ConsoleUpdateHistory class that all histories will
      extend from (analogous to HistoryEntry)
    
    RegistrarUpdateHistory and RegistrarPocUpdateHistory will look similar,
    but ConsoleEppActionHistory will only contain the linked history class
    type and ID

---
+++ db/src/main/resources/sql/flyway/V155__console_update_history.sql
@@ -0,0 +1,40 @@
+-- Copyright 2024 The Nomulus Authors. All Rights Reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+CREATE TABLE "UserUpdateHistory" (
+ history_revision_id int8 NOT NULL,
+ history_date_time timestamptz NOT NULL,
+ history_method text NOT NULL,
+ history_request_body text,
+ history_type text NOT NULL,
+ history_url text NOT NULL,
+ id int8 NOT NULL,
+ email_address text NOT NULL,
+ registry_lock_password_hash text,
+ registry_lock_password_salt text,
+ global_role text NOT NULL,
+ is_admin boolean NOT NULL,
+ registrar_roles hstore,
+ update_timestamp timestamptz,
+ history_acting_user text,
+ PRIMARY KEY (history_revision_id)
+);
+
+CREATE INDEX IDXbjacjlm8ianc4kxxvamnu94k5 ON "UserUpdateHistory" (history_acting_user);
+CREATE INDEX IDX5yqacw829y5bm6f7eajsq1cts ON "UserUpdateHistory" (email_address);
+
+ALTER TABLE IF EXISTS "UserUpdateHistory"
+    ADD CONSTRAINT FK1s7bopbl3pwrhv3jkkofnv3o0
+    FOREIGN KEY (history_acting_user)
+    REFERENCES "User" (email_address);
