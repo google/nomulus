e9b2fc4750a0cc0ab1b87789295f7386c08b55a4
Gus Brodman <gbrodman@google.com>
2024-02-20 15:01:43 -0500

    Add Java classes for console history objects
    This also creates base classes for the objects contained within the
    history classes, e.g. RegistrarBase. This is the same way that objects
    stored in the HistoryEntry subclasses have base classes, e.g.
    DomainBase.

--- core/src/test/java/google/registry/ui/server/registrar/RegistrarSettingsActionTestCase.java
+++ core/src/test/java/google/registry/ui/server/registrar/RegistrarSettingsActionTestCase.java
@@ -38,6 +38,7 @@ import com.google.common.truth.Truth;
 import google.registry.flows.certs.CertificateChecker;
 import google.registry.groups.GmailClient;
 import google.registry.model.registrar.RegistrarPoc;
+import google.registry.model.registrar.RegistrarPocBase;
 import google.registry.persistence.transaction.JpaTestExtensions;
 import google.registry.persistence.transaction.JpaTestExtensions.JpaIntegrationTestExtension;
 import google.registry.request.JsonActionRunner;
@@ -99,7 +100,7 @@ public abstract class RegistrarSettingsActionTestCase {
     // Add a technical contact to the registrar (in addition to the default admin contact created by
     // JpaTransactionManagerExtension).
     techContact =
-        getOnlyElement(loadRegistrar(CLIENT_ID).getContactsOfType(RegistrarPoc.Type.TECH));
+        getOnlyElement(loadRegistrar(CLIENT_ID).getContactsOfType(RegistrarPocBase.Type.TECH));
 
     action.registrarAccessor = null;
     action.jsonActionRunner =
