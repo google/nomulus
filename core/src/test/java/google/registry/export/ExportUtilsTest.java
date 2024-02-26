bd46dd1591c73749dd148a60a2831ec740569da7
Sarah Botwinick <sarahbot@google.com>
2024-02-26 16:18:02 -0500

    fix test comment

--- core/src/test/java/google/registry/export/ExportUtilsTest.java
+++ core/src/test/java/google/registry/export/ExportUtilsTest.java
@@ -43,12 +43,8 @@ class ExportUtilsTest {
         "tld-reserved2",
         "lol,NAME_COLLISION",
         "snow,FULLY_BLOCKED");
-    ReservedList rl3 = persistReservedList(
-        "tld-reserved3",
-        "tine,FULLY_BLOCKED");
     createTld("tld");
     persistResource(Tld.get("tld").asBuilder().setReservedLists(rl1, rl2, rl3).build());
-    // Should not contain jimmy or oval.
     assertThat(new ExportUtils("# This is a disclaimer.").exportReservedTerms(Tld.get("tld")))
         .isEqualTo("# This is a disclaimer.\ncat\nlol\nsnow\ntine\n");
   }
