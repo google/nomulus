e31bc4f873776c131a3b69d29c75e03c3a66c280
ptkach <ptkach@google.com>
2024-04-11 18:33:44 +0000

    Add console DUM download

--- core/src/test/java/google/registry/ui/server/console/ConsoleDUMDownloadActionTest.java
+++ core/src/test/java/google/registry/ui/server/console/ConsoleDUMDownloadActionTest.java
@@ -1,3 +1,17 @@
+// Copyright 2024 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package google.registry.ui.server.console;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -5,6 +19,7 @@ import static google.registry.testing.DatabaseHelper.createTld;
 import static org.mockito.Mockito.when;
 
 import com.google.api.client.http.HttpStatusCodes;
+import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
 import google.registry.model.console.GlobalRole;
 import google.registry.model.console.User;
@@ -24,7 +39,6 @@ import org.joda.time.DateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.testcontainers.shaded.com.google.common.collect.ImmutableList;
 
 class ConsoleDUMDownloadActionTest {
 
