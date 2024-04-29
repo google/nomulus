ca681d344bbca0d151661038cc985154a14bed01
Gus Brodman <gbrodman@google.com>
2024-04-29 13:38:36 -0400

    Add registryLockEmailAddress field to User object
    We've added the field in the database in a previous PR. This is only
    used in the old console for now because the new console does not have
    registry lock functionality yet

--- core/src/main/java/google/registry/model/console/User.java
+++ core/src/main/java/google/registry/model/console/User.java
@@ -26,6 +26,7 @@ import google.registry.model.Buildable;
 import google.registry.model.UpdateAutoTimestampEntity;
 import google.registry.persistence.VKey;
 import google.registry.util.PasswordUtils;
+import java.util.Optional;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -50,6 +51,9 @@ public class User extends UpdateAutoTimestampEntity implements Buildable {
   @Column(nullable = false)
   private String emailAddress;
 
+  /** Optional external email address to use for registry lock confirmation emails. */
+  @Column private String registryLockEmailAddress;
+
   /** Roles (which grant permissions) associated with this user. */
   @Column(nullable = false)
   private UserRoles userRoles;
@@ -69,6 +73,10 @@ public class User extends UpdateAutoTimestampEntity implements Buildable {
 
   public String getEmailAddress() {
     return emailAddress;
+  }
+
+  public Optional<String> getRegistryLockEmailAddress() {
+    return Optional.ofNullable(registryLockEmailAddress);
   }
 
   public UserRoles getUserRoles() {
@@ -133,6 +141,15 @@ public class User extends UpdateAutoTimestampEntity implements Buildable {
 
     public Builder setEmailAddress(String emailAddress) {
       getInstance().emailAddress = checkValidEmail(emailAddress);
+      return this;
+    }
+
+    public Builder setRegistryLockEmailAddress(String registryLockEmailAddress) {
+      if (registryLockEmailAddress == null) {
+        getInstance().registryLockEmailAddress = null;
+      } else {
+        getInstance().registryLockEmailAddress = checkValidEmail(registryLockEmailAddress);
+      }
       return this;
     }
 
