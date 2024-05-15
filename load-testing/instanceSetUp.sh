e42f7a9b20968fc6a2c890d265960c68e922b7f6
Sarah <sarahbot@google.com>
2024-05-15 16:02:48 -0400

    Add an instances setUp and cleanUp script

---
+++ load-testing/instanceSetUp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright 2024 The Nomulus Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Create the instances - modify this number for the amount of instnaces you
+# would like to use for your load test
+gcloud compute instances create loadtest-{1..2} --machine-type g1-small --zone us-east4-a
+
+# Get all the created load tests instances
+HOSTS=$(gcloud compute instances list | awk '/^loadtest/ { print $5 }')
+
+#Install rsync
+for host in $HOSTS;
+  do
+    for i in {1..60}; do
+      if ssh $host sudo apt-get -y install rsync; then
+        ssh $host mkdir test-client/
+        break
+      else
+        sleep 5
+      fi
+    done
+  done
+
+#Install Java 21
+tar -xvf load-testing/jdk-21_linux-x64_bin.tar.gz &&
+for host in $HOSTS; do rsync -avz jdk-21.0.2/ $host:test-client/jdk-21.0.2/; done
+rm -rf jdk-21.0.2/
