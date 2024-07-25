8795777be96ad60daec3bc0905fd29aaae327aca
Gus Brodman <gbrodman@google.com>
2024-07-25 15:04:27 -0400

    Change pkey of User to emailAddress
    Originally, we though that User entities were going to have mutable
    email addresses, and thus would require a non-changing primary key. This
    proved to not be the case. It'll simplify the User loading/saving code
    if we just do everything by email address.
    
    Obviously this doesn't change much functionality, but it prepares us for
    removing the id field down the line once the changes propagate.

--- core/src/main/java/google/registry/model/console/UserDao.java
+++ core/src/main/java/google/registry/model/console/UserDao.java
@@ -14,7 +14,6 @@
 
 package google.registry.model.console;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static google.registry.persistence.transaction.TransactionManagerFactory.tm;
 
 import java.util.Optional;
@@ -34,21 +33,6 @@ public class UserDao {
 
   /** Saves the given user, checking that no existing user already exists with this email. */
   public static void saveUser(User user) {
-    tm().transact(
-            () -> {
-              // Check for an existing user (the unique constraint protects us, but this gives a
-              // nicer exception)
-              Optional<User> maybeSavedUser = loadUser(user.getEmailAddress());
-              if (maybeSavedUser.isPresent()) {
-                User savedUser = maybeSavedUser.get();
-                checkArgument(
-                    savedUser.getId().equals(user.getId()),
-                    String.format(
-                        "Attempted save of User with email address %s and ID %s, user with that"
-                            + " email already exists with ID %s",
-                        user.getEmailAddress(), user.getId(), savedUser.getId()));
-              }
-              tm().put(user);
-            });
+    tm().transact(() -> tm().put(user));
   }
 }
