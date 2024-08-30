4acf88504a1a77d6e9e7e81fae6222bb69ce0525
Juan Celhay <jicelhay@google.com>
2024-08-30 15:02:34 -0400

    fix formatting

--- core/src/test/java/google/registry/storage/drive/DriveConnectionTest.java
+++ core/src/test/java/google/registry/storage/drive/DriveConnectionTest.java
@@ -188,7 +188,8 @@ class DriveConnectionTest {
     assertThat(thrown.getCause()).isEqualTo(googleJsonResponseException);
     assertThat(thrown.getMessage())
         .isEqualTo(
-            "Max. failures reached while attempting to list Drive files in folder driveFolderId with query sampleQuery; failing permanently.");
+            "Max. failures reached while attempting to list Drive files in folder "
+                + "driveFolderId with query sampleQuery; failing permanently.");
 
     verify(filesList, times(0)).setPageToken(null);
     verify(filesList, times(1)).setQ("'driveFolderId' in parents and sampleQuery");
