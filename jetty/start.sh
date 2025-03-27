373b7aacd6e9ea9ddc862c1616ba9442b4872593
ptkach <ptkach@google.com>
2025-03-27 13:55:27 -0400

    Update jetty console static files cache policies

--- jetty/start.sh
+++ jetty/start.sh
@@ -15,7 +15,70 @@
 
 env=${1:-"alpha"}
 cd /jetty-base
+mkdir -p webapps/console-${env}/WEB-INF
 cp -rf webapps/console-${env} webapps/console
+cd webapps
+
+# Remove all environment builds not used in the deployment
+find . -maxdepth 1 -type d -name "console-*" -exec rm -rf {} +
+
+# Configure cache policies for Registry Console static resources
+echo '<web-app xmlns="https://jakarta.ee/xml/ns/jakartaee"
+               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               xsi:schemaLocation="https://jakarta.ee/xml/ns/jakartaee https://jakarta.ee/xml/ns/jakartaee/web-app_5_0.xsd"
+               version="5.0">
+
+            <servlet>
+                <servlet-name>default-no-cache</servlet-name>
+                <servlet-class>org.eclipse.jetty.ee10.servlet.DefaultServlet</servlet-class>
+                <init-param>
+                    <param-name>cacheControl</param-name>
+                    <param-value>no-cache, no-store, must-revalidate</param-value>
+                </init-param>
+            </servlet>
+
+            <servlet>
+                <servlet-name>default-cache-static</servlet-name>
+                <servlet-class>org.eclipse.jetty.ee10.servlet.DefaultServlet</servlet-class>
+                <init-param>
+                    <param-name>cacheControl</param-name>
+                    <param-value>public, max-age=31536000</param-value>
+                </init-param>
+            </servlet>
+
+            <servlet-mapping>
+                <servlet-name>default-no-cache</servlet-name>
+                <url-pattern>*.html</url-pattern>
+            </servlet-mapping>
+
+            <servlet-mapping>
+                <servlet-name>default-no-cache</servlet-name>
+                <url-pattern>main.js</url-pattern>
+            </servlet-mapping>
+
+            <servlet-mapping>
+                <servlet-name>default-no-cache</servlet-name>
+                <url-pattern>styles.css</url-pattern>
+            </servlet-mapping>
+
+            <servlet-mapping>
+                <servlet-name>default-cache-static</servlet-name>
+                <url-pattern>*.css</url-pattern>
+            </servlet-mapping>
+
+            <servlet-mapping>
+                <servlet-name>default-cache-static</servlet-name>
+                <url-pattern>*.js</url-pattern>
+            </servlet-mapping>
+
+            <servlet-mapping>
+                <servlet-name>default-cache-static</servlet-name>
+                <url-pattern>*.png</url-pattern>
+            </servlet-mapping>
+
+    </web-app>' > console/WEB-INF/web.xml
+
+cd /jetty-base
 echo "Running ${env}"
 java -Dgoogle.registry.environment=${env} \
     -Djava.util.logging.config.file=/logging.properties \
