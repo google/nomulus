04947977714e3ccf6623c3317d11735c564f1ae9
ptkach <ptkach@google.com>
2025-04-01 20:54:05 +0000

    Add GKE readiness probe

--- core/src/main/java/google/registry/module/RequestComponent.java
+++ core/src/main/java/google/registry/module/RequestComponent.java
@@ -58,6 +58,8 @@ import google.registry.flows.TlsCredentials.EppTlsModule;
 import google.registry.flows.custom.CustomLogicModule;
 import google.registry.loadtest.LoadTestAction;
 import google.registry.loadtest.LoadTestModule;
+import google.registry.module.ReadinessProbeAction.ReadinessProbeActionPubApi;
+import google.registry.module.ReadinessProbeAction.ReadinessProbeConsoleAction;
 import google.registry.monitoring.whitebox.WhiteboxModule;
 import google.registry.rdap.RdapAutnumAction;
 import google.registry.rdap.RdapDomainAction;
@@ -254,6 +256,10 @@ interface RequestComponent {
   PublishInvoicesAction uploadInvoicesAction();
 
   PublishSpec11ReportAction publishSpec11ReportAction();
+
+  ReadinessProbeConsoleAction readinessProbeConsoleAction();
+
+  ReadinessProbeActionPubApi readinessProbeActionPubApi();
 
   RdapAutnumAction rdapAutnumAction();
 
