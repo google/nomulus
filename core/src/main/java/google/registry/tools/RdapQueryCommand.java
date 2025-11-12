53d60ed3b0061039e04f4314a9cd47a5e0f07e49
Harshita Sharma <harshitagoogle@google.com>
2025-11-12 15:22:10 +0000

    feat: Implement rdap_query command

--- core/src/main/java/google/registry/tools/RdapQueryCommand.java
+++ core/src/main/java/google/registry/tools/RdapQueryCommand.java
@@ -1,104 +1,88 @@
-// Copyright 2025 The Nomulus Authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package google.registry.tools;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.google.common.base.Ascii;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.flogger.GoogleLogger;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import google.registry.config.RegistryConfig.Config;
+import google.registry.request.Action.GkeService;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+import javax.inject.Inject;
 
-/** Command to execute an authenticated RDAP query. */
+/** Command to manually perform an authenticated RDAP query. */
 @Parameters(separators = " =", commandDescription = "Manually perform an authenticated RDAP query")
 public final class RdapQueryCommand implements CommandWithConnection {
 
-  private static final ImmutableSet<String> VALID_TYPES =
-      ImmutableSet.of("domain", "nameserver", "entity");
+  private static final GoogleLogger logger = GoogleLogger.forEnclosingClass();
 
-  @Parameter(description = "RDAP query string, in the format <type> <name>", required = true)
+  private static final ImmutableSet<String> VALID_TYPES =
+      ImmutableSet.of("domain", "registrar", "contact", "nameserver");
+
+  @Parameter(description = "The object type and query term.", required = true)
   private List<String> mainParameters;
 
-  private ServiceConnection connection;
+  private ServiceConnection defaultConnection;
+
+  @Inject
+  @Config("useCanary")
+  boolean useCanary;
 
   @Override
   public void setConnection(ServiceConnection connection) {
-    this.connection = connection;
+    this.defaultConnection = connection;
   }
 
   @Override
   public void run() {
-    if (mainParameters == null || mainParameters.size() != 2) {
-      throw new IllegalArgumentException(
-          "Usage: nomulus rdap_query <type> <query_term>\n"
-              + "  <type> must be one of "
-              + VALID_TYPES);
-    }
+    checkArgument(
+        mainParameters != null && mainParameters.size() == 2,
+        "Usage: nomulus rdap_query <type> <query_term>\n"
+            + "  <type> must be one of "
+            + VALID_TYPES);
 
     String type = Ascii.toLowerCase(mainParameters.get(0));
-    if (!VALID_TYPES.contains(type)) {
-      throw new IllegalArgumentException(
-          String.format("Invalid object type '%s'. Must be one of %s", type, VALID_TYPES));
-    }
+    checkArgument(
+        VALID_TYPES.contains(type),
+        "Invalid object type '%s'. Must be one of %s",
+        type,
+        VALID_TYPES);
+
     String name = mainParameters.get(1);
     String path = String.format("/rdap/%s/%s", type, name);
 
+    logger.atInfo().log("Starting RDAP query for path: %s", path);
+
     try {
-      String rdapResponse = connection.sendGetRequest(path, ImmutableMap.of());
+      if (defaultConnection == null) {
+        throw new IllegalStateException("ServiceConnection was not set by RegistryCli.");
+      }
+      // Create a new ServiceConnection instance targeting GkeService.PUBAPI.
+      // This is necessary because the default connection provided by RegistryCli
+      // targets GkeService.BACKEND, but RDAP queries need to be routed to the
+      // public-facing API.
+      ServiceConnection pubapiConnection =
+          defaultConnection.withService(GkeService.PUBAPI, useCanary);
+
+      String rdapResponse = pubapiConnection.sendGetRequest(path, ImmutableMap.of());
       JsonElement rdapJson = JsonParser.parseString(rdapResponse);
-      System.out.println(formatJsonElement(rdapJson, ""));
+
+      // Pretty-print the JSON response.
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      System.out.println(gson.toJson(rdapJson));
+
+      logger.atInfo().log("Successfully completed RDAP query for path: %s", path);
     } catch (IOException e) {
+      logger.atSevere().withCause(e).log("Request failed for path: %s", path);
       System.err.println("Request failed for " + path + ": " + e.getMessage());
     }
-  }
-
-  /** Recursively formats a JSON element into indented key-value pairs. */
-  private String formatJsonElement(JsonElement element, String indent) {
-    StringBuilder sb = new StringBuilder();
-    if (element == null || element.isJsonNull()) {
-      // Omit nulls for cleaner output
-    } else if (element.isJsonObject()) {
-      JsonObject obj = element.getAsJsonObject();
-      obj.entrySet().stream()
-          .sorted(Map.Entry.comparingByKey()) // Sort keys for consistent output
-          .forEach(
-              entry -> {
-                if (!entry.getValue().isJsonNull()) {
-                  sb.append(indent).append(entry.getKey()).append(":\n");
-                  sb.append(formatJsonElement(entry.getValue(), indent + "  "));
-                }
-              });
-    } else if (element.isJsonArray()) {
-      JsonArray array = element.getAsJsonArray();
-      for (JsonElement arrayElement : array) {
-        if (arrayElement.isJsonPrimitive()) {
-          sb.append(indent).append("- ").append(arrayElement.getAsString()).append("\n");
-        } else {
-          sb.append(formatJsonElement(arrayElement, indent + "  "));
-        }
-      }
-    } else if (element.isJsonPrimitive()) {
-      sb.append(indent).append(element.getAsString()).append("\n");
-    }
-    return sb.toString();
   }
 }
