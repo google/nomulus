8a390b997dee5e1d8edb6506b014e881d7d88654
Harshita Sharma <harshitagoogle@google.com>
2025-11-24 15:34:23 +0000

    modifying and correcting issues

--- core/src/test/java/google/registry/tools/RdapQueryCommandTest.java
+++ core/src/test/java/google/registry/tools/RdapQueryCommandTest.java
@@ -28,9 +28,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 /** Unit tests for {@link RdapQueryCommand}. */
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class RdapQueryCommandTest extends CommandTestCase<RdapQueryCommand> {
 
   @Mock private ServiceConnection mockDefaultConnection;
