50001001171a99114f02577bcd8536e5f37bdbef
Weimin Yu <weiminyu@google.com>
2026-01-30 23:39:29 +0000

    Normalize Fee extension XML tags in EPP response
    Nomulus currently supports multiple versions of the Fee extensions. Our
    current tooling requires that each version must use a unique namespace
    tag, e.g., fee11, fee12, etc.
    
    Some client registrars are sensitive to the tag literal used by the
    version of the extension they use. For example, a few registrars
    currently using v0.6 have requested that the `fee` literal be used
    on the versions they currently use. With registrars upgrading at their
    own schedule, this kind of requests are impossible to satisfy.
    
    This PR instroduces a namespace normalizer class for EPP responses. The
    key optimization is that each EPP response never mixes multiple versions
    of a service extension. Therefore we can define a canonical tag for each
    extension, and change the tag of the extension in use in a response to
    that. This normalizer only handles Fee extensions right now, but the
    idea can be extended to others if use cases come up.
    
    This normalizer will be applied to all flows in a future PR.

---
+++ core/src/test/java/google/registry/flows/FeeExtensionXmlTagNormalizerTest.java
@@ -0,0 +1,73 @@
+// Copyright 2026 The Nomulus Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.package google.registry.flows;
+
+package google.registry.flows;
+
+import static com.google.common.truth.Truth.assertThat;
+import static google.registry.flows.FeeExtensionXmlTagNormalizer.normalizeFeeExtensionTag;
+import static google.registry.model.eppcommon.EppXmlTransformer.validateOutput;
+import static google.registry.testing.TestDataHelper.loadBytes;
+import static google.registry.testing.TestDataHelper.loadFile;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class FeeExtensionXmlTagNormalizerTest {
+
+  @ParameterizedTest
+  @MethodSource("provideTestCombinations")
+  @SuppressWarnings("unused") // Parameter 'name' is part of test case name
+  void success_withFeeExtension(String name, String inputXmlFilename, String expectedXmlFilename)
+      throws Exception {
+    byte[] xmlBytes = loadBytes(getClass(), inputXmlFilename).read();
+    String normalized = normalizeFeeExtensionTag(xmlBytes);
+    String expected = loadFile(getClass(), expectedXmlFilename);
+    // Verify that expected xml is syntatically correct.
+    validateOutput(expected);
+
+    assertThat(normalized).isEqualTo(expected);
+  }
+
+  @Test
+  void success_noFeeExtensions() throws Exception {
+    byte[] xmlBytes = loadBytes(getClass(), "domain_create.xml").read();
+    String normalized = normalizeFeeExtensionTag(xmlBytes);
+    assertThat(normalized).isEqualTo(new String(xmlBytes, UTF_8));
+  }
+
+  @SuppressWarnings("unused")
+  static Stream<Arguments> provideTestCombinations() {
+    return Stream.of(
+        Arguments.of(
+            "v06",
+            "domain_check_fee_response_raw_v06.xml",
+            "domain_check_fee_response_normalized_v06.xml"),
+        Arguments.of(
+            "v11",
+            "domain_check_fee_response_raw_v11.xml",
+            "domain_check_fee_response_normalized_v11.xml"),
+        Arguments.of(
+            "v12",
+            "domain_check_fee_response_raw_v12.xml",
+            "domain_check_fee_response_normalized_v12.xml"),
+        Arguments.of(
+            "stdv1",
+            "domain_check_fee_response_raw_stdv1.xml",
+            "domain_check_fee_response_normalized_stdv1.xml"));
+  }
+}
